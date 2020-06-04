65e387e21636dfda102759ea3ca205ea7667f184
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-04 15:49:22 -0300

    input text in Searchbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar type="text" />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,27 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        searchText:'',
+    };
+  }
+
+  onSearchTextChange = event => {
+      this.setState({
+          search: event.target.value
+      });
+  }
+
+  render() {
+    return (
+        <div>
+            <label for="search-bar">Inclui o texto:</label>
+            <input type="text" name="search-bar" value={this.state.searchText} onChange={this.onSearchTextChange} ></input>
+        </div>
+    );
+  }
+}
+
+export default SearchBar;
