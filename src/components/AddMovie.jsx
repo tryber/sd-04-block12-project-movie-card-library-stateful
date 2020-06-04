fcf372f57ab682db26668de958068dc0894d65bc
cacobribeiro <cacobribeiro@gmail.com>
2020-06-04 18:06:16 -0300

    Teste CC 4

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -27,12 +27,40 @@ export default class AddMovie extends React.Component {
     return (
       <div>
         <form>
-          <label htmlFor="titulo">Título<input type="text" onChange={this.addMovie} value={title} /></label>
-          <label htmlFor="subtitulo">Subtítulo<input type="text" onChange={this.addMovie} value={subtitle} /></label>
-          <label htmlFor="imagepath">Imagem<input type="text" onChange={this.addMovie} value={imagePath} /></label>
-          <label htmlFor="storyline">Sinopse<input type="textarea" onChange={this.addMovie} value={storyline} /></label>
-          <label htmlFor="rating">Avaliação<input type="text" onChange={this.addMovie} value={rating} /></label>
-          <label htmlFor="genre">Gênero<input type="text" onChange={this.addMovie} value={genre} /></label>
+          <label htmlFor="titulo">Título<input
+            type="text"
+            onChange={this.addMovie}
+            value={title} />
+          </label>
+          <label htmlFor="subtitulo">Subtítulo<input
+            type="text"
+            onChange={this.addMovie}
+            value={subtitle}
+          />
+          </label>
+          <label htmlFor="imagepath">Imagem<input
+            type="text"
+            onChange={this.addMovie}
+            value={imagePath}
+          />
+          </label>
+          <label htmlFor="storyline">Sinopse<input
+            type="textarea"
+            onChange={this.addMovie}
+            value={storyline}
+          />
+          </label>
+          <label htmlFor="rating">Avaliação<input type="text"
+            onChange={this.addMovie}
+            value={rating}
+          />
+          </label>
+          <label htmlFor="genre">Gênero<input
+            type="text"
+            onChange={this.addMovie}
+            value={genre}
+          />
+          </label>
         </form>
       </div >
     );
