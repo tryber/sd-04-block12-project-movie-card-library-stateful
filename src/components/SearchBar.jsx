fabeb309653ad4538f1bbc679b508e4817372cd3
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-04 15:14:46 -0300

    tentando

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,13 +3,24 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { serachText, onSearchTextChange,
-      BookmarkedChange, selectedGenre,
-      onSelectedGenreChange } = this.props;
+    const { 
+      searchText, onSearchTextChange, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange, bookmarkedOnly,
+    } = this.props;
     return (
-      <div>
-        
-      </div>
+      <form>
+        <label id='inputOne'>**"Inclui o texto:"**</label>
+        <input type='text' id='inputOne' value={searchText} onChange={onSearchTextChange} />
+        <label id='inputTwo'>**"Mostrar somente favoritos"**</label>
+        <input id='inputTwo' type='checkbox' checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        <label id='inputThree'>**"Filtrar por gênero"**</label>
+        <select id='inputThree' value={selectedGenre} onChange={onSelectedGenreChange}>
+          <option value=''>Todos</option>
+          <option value='action'>Ação</option>
+          <option value='comedy'>Comédia</option>
+          <option value='thriller'>Suspense</option>
+        </select>
+      </form>
     );
   }
 }
