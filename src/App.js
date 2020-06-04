2f8da7d18e41e319c20ce4a8e433fa0d01a1e19b
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-04 19:02:50 -0300

    searchbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import SearchBar from './components/SearchBar'
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar/>
+
     </div>
+   
+
   );
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,50 @@
 // implement SearchBar component here
+import React from 'react';
+
+const genres = [{ value: '', text: 'Todos' },
+{ value: 'action', text: 'Ação' },
+{ value: 'comedy', text: 'Comédia' },
+{ value: 'thriller', text: 'Suspense' },]
+export class SearchBar extends React.Component {
+
+  render() {
+    return (
+      <form>
+        <label>
+          Inclui o texto
+          <input
+            type="text"
+            name="searchText"
+            value={this.props.searchText}
+            onChange={this.props.onSearchTextChange}
+          />
+        </label>
+        <label>
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name="searchText"
+            checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange}
+          />
+        </label>
+        <label>
+          Filtrar por gênero
+          <select
+            name="searchText"
+            value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange}
+          >
+            {genres.map(genre => (
+              <option key={genre.value} value={genre.value}>
+                {genre.text}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
