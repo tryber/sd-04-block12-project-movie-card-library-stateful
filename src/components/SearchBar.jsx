21d57dadb3f9c52cc61cd71f5c11ca96f9c97225
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-04 16:54:24 -0300

    select genre ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,38 +6,59 @@ class SearchBar extends Component {
     this.state = {
       searchText: '',
       bookMarkedOnly: false,
+      selectGenre: "",
     };
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectGenreChange = this.onSelectGenreChange.bind(this);
   }
 
   onSearchTextChange(event) {
-    this.setState(
-      {
-        search: event.target.value
-      }
-    );
+    this.setState({
+        search: event.target.value,
+    });
   }
 
-  onBookmarkedChange() {
-    this.setState(
-      {
-    //   bookMarkedOnly: event.taget.value
-      }
-    );
+  onBookmarkedChange(event) {
+    this.setState({
+      bookMarkedOnly: event.target.value
+    });
+  }
+
+  onSelectGenreChange(event) {
+    this.setState({
+      selectGenre: event.target.value
+    });
   }
 
   render() {
     return (
       <section>
-          <div>
-            <label htmlFor="search-bar">Inclui o texto:</label>
-            <input type="text" name="search-bar" value={this.state.searchText} onChange={this.onSearchTextChange} ></input>
-          </div>
-          <div>
-            <input type="checkbox" name="checkbox" checked={this.state.bookMarkedOnly} onChange={this.onBookmarkedChange} />
-            <label htmlFor="checkbox">Mostrar somente favoritos</label>
-          </div>
+        <div>
+          <label htmlFor="search-bar">Inclui o texto:</label>
+          <input 
+            type="text" 
+            name="search-bar" 
+            value={this.state.searchText} 
+            onChange={this.onSearchTextChange} />
+        </div>
+        <div>
+          <input 
+            type="checkbox" 
+            name="checkbox" 
+            checked={this.state.bookMarkedOnly} 
+            onChange={this.onBookmarkedChange} />
+          <label htmlFor="checkbox">Mostrar somente favoritos</label>
+        </div>
+        <div>
+          <label htmlFor="select-genre">Filtrar por genero</label>
+          <select name="select=-genre" onChange={this.onSelectGenreChange}>
+            <option selected value={this.state.selectGenre}>Todos</option>
+            <option value="action">Acao</option>
+            <option value="comedy">Comedia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </div>
       </section>
     );
   }
