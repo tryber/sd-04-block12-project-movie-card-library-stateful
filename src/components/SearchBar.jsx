cecbeecc2ed5372d8d55eb0fc24d2a06f5967cb0
Leandro Marten <leandromarten1@gmail.com>
2020-06-04 20:49:35 -0300

    Favor CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,20 +9,21 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form>
-        <label htmlFor="searchText">Inclui o texto:</label><br />
-        <input name="searchText" type="text"
-        value={searchText} onChange={onSearchTextChange} /><br />
-        <input name="bookmarkedOnly" type="checkbox"
-        defaultChecked={bookmarkedOnly} onChange={onBookmarkedChange} />
-        <label htmlFor="bookmarkedOnly">Mostrar somente favoritos</label><br />
-        <label htmlFor="genre">Filtrar por gênero<br />
-          {<select name="selectedGenre"
-          defaultValue={selectedGenre} onChange={onSelectedGenreChange}>
+        <label htmlFor="searchText">Inclui o texto:</label><br/>
+        <input 
+          name="searchText"type="text" value={searchText} onChange={onSearchTextChange}
+        /><br/>
+        <input
+          name="bookmarkedOnly" type="checkbox" defaultChecked={bookmarkedOnly} onChange={onBookmarkedChange}
+        />
+        <label htmlFor="bookmarkedOnly">Mostrar somente favoritos</label><br/>
+        <label htmlFor="genre">Filtrar por gênero<br/>
+          <select name="selectedGenre" defaultValue={selectedGenre} onChange={onSelectedGenreChange}>
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
             <option value="thriller">Suspense</option>
-          </select>}
+          </select>
         </label>
       </form>
     );
