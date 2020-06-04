d2315e37b8bc9a839628baa02497d37987f195c5
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-06-04 14:49:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 
 import Header from './components/Header';
 
-function App() {
+export default () {
   return (
     <div className="App">
       <Header />
     </div>
   );
 }
-
-export default App;
