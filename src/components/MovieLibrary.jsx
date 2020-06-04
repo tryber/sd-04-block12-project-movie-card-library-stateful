164967d0291bfec41bea042632766d5c526c0511
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-06-04 18:57:47 -0300

    teste

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,45 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+    this.textChange = this.textChange.bind(this);
+    this.checkChange = this.checkChange.bind(this);
+    this.selectChange = this.selectChange.bind(this);
+  }
+
+  textChange(e) {
+    this.setState({ searchText: e.target.value });
+  }
+
+  checkChange() {
+    if (this.state.bookmarkedOnly === false) {
+      this.setState({ bookmarkedOnly: true });
+    } else this.setState({ bookmarkedOnly: false });
+  }
+
+  selectChange(e) {
+    this.setState({ selectedGenre: e.target.value });
+  }
+
+  render() {
+    return (
+      <SearchBar
+        searchText={this.state.searchText}
+        onSearchTextChange={this.textChange}
+        bookmarkedOnly={this.state.bookmarkedOnly}
+        onBookmarkedChange={this.checkChange}
+        selectedGenre={this.state.selectedGenre}
+        onSelectedGenreChange={this.selectChange}
+      />
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,51 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form>
+        <label>
+          Inclui o texto:
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
+        </label>
+        <label>
+          Mostrar somente favoritos
+          <input
+            name="bookmarkedOnly"
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          />
+        </label>
+
+        <label>
+          Filtrar por gênero
+          <select
+            name="selectedGenre"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+          >
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
