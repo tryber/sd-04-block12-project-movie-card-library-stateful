30931f488975b8319a12c836ef41dbb8e9d8dd2c
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-04 19:31:14 -0300

    CC 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,34 +4,33 @@ import React from 'react';
 const genres = [{ value: '', text: 'Todos' },
 { value: 'action', text: 'Ação' },
 { value: 'comedy', text: 'Comédia' },
-{ value: 'thriller', text: 'Suspense' } ];
+{ value: 'thriller', text: 'Suspense' }];
 
 export class SearchBar extends React.Component {
 
   render() {
     return (
       <form>
-        <label htmlFor="01">Inclui o texto<input
-            type="text"
-            value={this.props.searchText}
-            onChange={this.props.onSearchTextChange}/>
+        <label htmlFor="01">Inclui o texto
+        <input
+            type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}
+          />
         </label>
-        <label htmlFor="02">Mostrar somente favoritos<input
-            type="checkbox"
-            checked={this.props.bookmarkedOnly}
-            onChange={this.props.onBookmarkedChange}/>
+        <label htmlFor="02">Mostrar somente favoritos
+        <input
+            type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}
+          />
         </label>
-        <label htmlFor="03">
-          Filtrar por gênero<select
-            value={this.props.selectedGenre}
-            onChange={this.props.onSelectedGenreChange}>
+        <label htmlFor="03"> Filtrar por gênero
+          <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}
+          >
             {genres.map((genre) => (
               <option key={genre.value} value={genre.value}>
                 {genre.text}</option>
             ))}
           </select>
         </label>
-        </form>
+      </form>
     );
   }
 }
