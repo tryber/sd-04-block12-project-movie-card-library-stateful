4b313e4fde7552528d8e2574ac3a9cd3019322e2
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-04 20:46:14 -0300

    CC

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,10 +5,9 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+  // constructor(props) {
+  //   super(props);
+  // }
   render() {
     return (
       <div>
