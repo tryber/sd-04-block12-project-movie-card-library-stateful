77ee74e5bbe50519cfa3af8eb7c561b52a054025
joaosoutto <joaosoutto@hotmail.com>
2020-06-04 19:13:10 -0300

    quale cc, agora ce aceita?

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -22,76 +22,25 @@ class AddMovie extends React.Component {
         storyline: '',
         rating: 0,
         genre: 'action',
-      })
+      });
   }
-  
   render() {
     const { onClick } = this.props;
     return (
       <form className="addMovie">
-        <label htmlFor="title">Título
-        <input
-          type="text"
-          name="title"
-          value={this.state.title}
-          onChange={(e) => this.setState({ title: e.target.value })}
-        />
-        </label>
-
-        <label htmlFor="subtitle">Subtítulo
-        <input
-          type="text"
-          name="subtitle"
-          value={this.state.subtitle}
-          onChange={(e) => this.setState({ subtitle: e.target.value })}
-        />
-        </label>
-
-        <label htmlFor="imagePath">Imagem
-        <input
-          type="text"
-          name="imagePath"
-          value={this.state.imagePath}
-          onChange={(e) => this.setState({ imagePath: e.target.value })}
-        />
-        </label>
-
-        <label htmlFor="storyline">Sinopse
-        <textarea
-        name="storyline"
-          value={this.state.storyline}
-          onChange={(e) => this.setState({ storyline: e.target.value })}
-        />
-        </label>
-
-        <label htmlFor="rating">Avaliação
-        <input
-          type="number"
-          name="rating"
-          value={this.state.rating}
-          onChange={(e) => this.setState({ rating: parseFloat(e.target.value) })}
-        />
-        </label>
-
-        <label htmlFor="genre">Gênero
-        <select
-        name="genre"
-          value={this.state.genre}
-          onChange={(e) => this.setState({ genre: e.target.value })}
-        >
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
-        </label>
-        
-        <button
-            type="button"
-            onClick={() => {
+        <label htmlFor="title">Título<input type="text" name="title" value={this.state.title} onChange={(e) => this.setState({ title: e.target.value })} /></label>
+        <label htmlFor="subtitle">Subtítulo<input type="text" name="subtitle" value={this.state.subtitle} onChange={(e) => this.setState({ subtitle: e.target.value })} /></label>
+        <label htmlFor="imagePath">Imagem<input type="text" name="imagePath" value={this.state.imagePath} onChange={(e) => this.setState({ imagePath: e.target.value })} /></label>
+        <label htmlFor="storyline">Sinopse<textarea name="storyline" value={this.state.storyline} onChange={(e) => this.setState({ storyline: e.target.value })} /></label>
+        <label htmlFor="rating">Avaliação<input type="number" name="rating" value={this.state.rating} onChange={(e) => this.setState({ rating: parseFloat(e.target.value) })} /></label>
+        <label htmlFor="genre">Gênero<select name="genre" value={this.state.genre} onChange={(e) => this.setState({ genre: e.target.value })}>
+          <option value="action">Ação</option><option value="comedy">Comédia</option><option value="thriller">Suspense</option>
+        </select></label>
+        <button type="button" onClick={() => {
               onClick(this.state);
               this.restoreState();
             }}
-          >
+        >
             Adicionar filme
           </button>
       </form>
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
-import MovieList from './MovieList';
+// import React from 'react';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
 
-class MovieLibrary extends React.Component {
-  constructor(props) {
-    super(props);
+// class MovieLibrary extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: this.props.movies,
-    };
-  }
+//     this.state = {
+//       searchText: '',
+//       bookmarkedOnly: false,
+//       selectedGenre: '',
+//       movies: this.props.movies,
+//     };
+//   }
 
-  
-  render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
-    return (
-      <div>
-        <SearchBar
-          searchText={searchText}
-          bookmarkedOnly={bookmarkedOnly}
-          selectedGenre={selectedGenre}
-        />
-      </div>
-    );
-  }
-}
 
-export default MovieLibrary;
+//   render() {
+//     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+//     return (
+//       <div>
+//         <SearchBar
+//           searchText={searchText}
+//           bookmarkedOnly={bookmarkedOnly}
+//           selectedGenre={selectedGenre}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default MovieLibrary;
