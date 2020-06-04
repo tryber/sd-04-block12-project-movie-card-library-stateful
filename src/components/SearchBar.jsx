7f2dceb50f03f4ca6acecb654b717cde247084c6
Diego Campos <diegodiko05@gmail.com>
2020-06-04 16:06:09 -0300

    CC 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
   render() {
     const {
       searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange, } = this.props;
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form>
         <label htmlFor="searchText">
@@ -13,9 +13,7 @@ class SearchBar extends React.Component {
           <input name="searchText" type="text" value={searchText} onChange={onSearchTextChange} />
         </label>
         <label htmlFor="favorites">Mostrar somente favoritos
-          <input name="favorites" type="checkbox"
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange} />
+          <input name="favorites" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </label>
         <label htmlFor="genreFilter">Filtrar por gênero
           <select name="genreFilter" value={selectedGenre} onChange={onSelectedGenreChange}>
