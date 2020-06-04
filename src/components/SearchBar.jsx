15179cd0e5f18095fa8e57311054e48414135490
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-04 15:55:40 -0300

    CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { 
-      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
     } = this.props;
     return (
       <form>
-        <label>Inclui o texto:
+        <label for="text">Inclui o texto:
           <input type="text" value={searchText} onChange={onSearchTextChange} />
         </label>
-        <label>Mostrar somente favoritos
+        <label for="favorites">Mostrar somente favoritos
           <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </label>
-        <label>Filtrar por gênero
+        <label for="genre">Filtrar por gênero
           <select value={selectedGenre} onChange={onSelectedGenreChange}>
             <option value="">Todos</option>
             <option value="action">Ação</option>
@@ -21,7 +25,6 @@ class SearchBar extends Component {
             <option value="thriller">Suspense</option>
           </select>
         </label>
-
       </form>
     )
   }
