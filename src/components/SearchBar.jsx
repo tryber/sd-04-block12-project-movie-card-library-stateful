3cdcabf9f9913fdbc76f277da2a2dcb9ffafe582
samueap <samueap@gmail.com>
2020-06-04 16:24:04 -0300

    add text SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+    } = this.props;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="text">Inclui o texto:</label>
+          <input type="text" value={searchText} name="text" onChange={onSearchTextChange} />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
