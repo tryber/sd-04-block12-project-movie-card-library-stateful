7a319775fd13cad76523b8d9daf3e4d94ae10d12
douglaza <douglaza@gmail.com>
2020-06-04 17:02:42 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
+import SearchBar from './components/SearchBar';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+// const searchText = '';
+// const selectedGenre = '';
+// const bookmarkedOnly = true;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      // onBookmarkedChange,
+      selectedGenre: '',
+      // onSelectedGenre,
+    };
+  }
+  onBookmarkedChange = () => {
+    console.log('onBookmarkedChange');
+  };
+  onSelectedGenre = () => {
+    console.log('onSelectedGenre');
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          searchText={this.state.searchText}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          selectedGenre={this.state.selectedGenre}
+          onBookmarkedChange={this.state.onBookmarkedChange}
+          onSelectedGenre={this.state.onSelectedGenre}
+        />
+        {/* <SearchBar searchOptions={(searchText, selectedGenre, bookmarkedOnly)} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,53 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div>
+        {/* <p>Requisito1</p> */}
+        <form>
+          <fieldset>
+            <label htmlFor="textInclude">
+              Inclui o texto:
+              <input
+                tybe="text"
+                name="textInclude"
+                value={this.props.searchText}
+                onChange={onSearchTextChange}
+              ></input>
+            </label>
+            <label htmlFor="onlyFavorites">
+              Mostrar somente favoritos:
+              <input
+                type="checkbox"
+                name="onlyFavorites"
+                value={this.props.searchText}
+                onChange={onBookmarkedChange}
+              ></input>
+            </label>
+            <label htmlFor="genreFilter">
+              Filtrar por gênero:
+              <select id="estado" name="genreFilter" value={this.props.selectedGenre}>
+                <option value="">Todos</option>
+                <option value="action">Ação</option>
+                <option value="comedy">Comédia</option>
+                <option value="thriller">Suspense</option>
+              </select>
+            </label>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
