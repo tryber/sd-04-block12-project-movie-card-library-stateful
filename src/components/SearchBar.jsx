371d45290205dabd636bc47732413ac5346628b4
orlando-messias <programadorlando@gmail.com>
2020-06-04 18:31:47 -0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,29 +5,25 @@ export default class MovieLibrary extends React.Component {
   render() {
     return (
       <div>
-        <div>
           <span>Inclui o texto:</span>
           <input
             type="text" name="search" value={this.props.searchText}
             onChange={this.props.onSearchTextChange}
           />
-        </div>
-        <div>
-          <input
-            type="checkbox" name="bookmark" checked={this.props.bookmarkedOnly}
-            onChange={this.props.onBookmarkedChange}
-          />
-          <span>Mostrar somente favoritos</span>
-        </div>
-        <div>
-          <span>Filtrar por gênero</span>
-          <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="commedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </div>
+          <br />
+        <input
+          type="checkbox" name="bookmark" checked={this.props.bookmarkedOnly}
+          onChange={this.props.onBookmarkedChange}
+        />
+        <span>Mostrar somente favoritos</span>
+        <br />
+        <span>Filtrar por gênero</span>
+        <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="commedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
       </div>
     );
   }
