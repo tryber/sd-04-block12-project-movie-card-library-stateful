f027d9352b6769a3ae9c5df603d7636c9192249b
Caio <caio@localhost.localdomain>
2020-06-04 18:54:55 -0300

    Implements SearchMovie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,38 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    console.log(`${name} ${value}`);
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { title } = this.state;
+    return (
+      <form>
+        <label>
+          Título
+          <input type="text" name="title" value={title} onChange={this.handleInput} />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,44 @@
-// implement SearchBar component here
+import React, { PureComponent } from 'react';
+
+class SearchBar extends PureComponent {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    const genres = {
+      Todos: '',
+      Ação: 'action',
+      Comédia: 'comedy',
+      Suspense: 'thriller',
+    };
+
+    return (
+      <form>
+        <label>
+          Inclui o texto
+          <input type="text" name="searchText" value={searchText} onChange={onSearchTextChange} />
+        </label>
+        <label>
+          Mostrar somente favoritos
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        </label>
+        <label>Filtrar por gênero</label>
+        <select value={selectedGenre} onChange={onSelectedGenreChange}>
+          {Object.keys(genres).map((genre) => (
+            <option key={genres[genre]} value={genres[genre]}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
