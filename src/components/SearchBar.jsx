db7d6a5f86693f54796a77f8d7c624b3b1dd390f
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-06-04 16:09:27 -0300

    changed class to function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,38 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class extends Component {
-
-  const { 
-    onSearchTextChange,
-    bookmarkedOnly, 
-    onBookmarkedChange, 
-    selectedGenre, 
-    onSelectedGenreChange 
-  } = this.props;
-
-  render() {
-    return (
-      <form>
-      
-        <label htmlFor="searchText">
-          Inclui o texto:
-          <input name="searchText" value={searchText} type="text" onChange={onSearchTextChange} />
-        </label>
-        <label htmlFor="onlyFavorites">
-          Mostrar somente favoritos
-          <input name="onlyFavorites" type="checkbox" checked={bookmrkedOnly} onChange={onBookmarkedChange} />
-        </label>
-        <label htmlFor="genreFilter">
-         <select name="genreFilter" value={selectedGenre}>
-          <option value="">Todos</option>
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-         </select>
-        </label>
-
-      </form>
-    )
-
-  };
-};
+export default ({
+  searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre,
+  onSelectedGenreChange,
+}) => (
+  <form>
+    <label htmlFor="searchText">
+      Inclui o texto:
+      <input name="searchText" value={searchText} type="text" onChange={onSearchTextChange} />
+    </label>
+    <label htmlFor="onlyFavorites">
+      Mostrar somente favoritos
+      <input name="onlyFavorites" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+    </label>
+    <label htmlFor="genreFilter">
+      Filtrar por gênero
+      <select name="genreFilter" value={selectedGenre} onChange={onSelectedGenreChange}>
+        <option value="">Todos</option>
+        <option value="action">Ação</option>
+        <option value="comedy">Comédia</option>
+        <option value="thriller">Suspense</option>
+      </select>
+    </label>
+  </form>
+);
