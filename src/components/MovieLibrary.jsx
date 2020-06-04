c9a4702053f04ee5ffe77f668b750b535e580947
orlando-messias <programadorlando@gmail.com>
2020-06-04 19:38:41 -0300

    MovieLibrary initial state

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,22 @@
 // implement MovieLibrary component here
+import React from 'react';
+
+export default class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+}
