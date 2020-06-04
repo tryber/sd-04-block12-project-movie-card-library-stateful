84b17724e7de790bb60e90835f3ae122be09c6ce
deboracosilveira <deboracosilveira@gmail.com>
2020-06-04 15:58:52 -0300

    SearchBar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,38 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchText: '',
+      onSearchTextChange: Function,
+      bookmarkedOnly: Boolean,
+      onBookmarkedChange: Function,
+      selectedGenre: '',
+      onSelectedGenreChange: Function
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.state.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.state.onBookmarkedChange}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.state.onSelectedGenreChange}
+        />
+        {/* <MovieList /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
