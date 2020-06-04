728888ed5b6530195c2af78ae1e296176ef5da04
procopio420 <lucas-procopio@outlook.com>
2020-06-04 18:30:31 -0300

    a

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,39 @@
 // implement SearchBar component here
 import React from 'react';
 
-const SearchBar = (props) => {
+const createInputField = (id, label, type, value, checked, onChange) => (
+  <label htmlFor={id}>
+    {label}
+    {type === 'checkbox' ? (
+      <input id={id} type={type} checked={checked} onChange={onChange} />
+    ) : (
+      <input id={id} type={type} value={value} onChange={onChange} />
+    )}
+  </label>
+);
+
+const createSelectField = (id, label, value, onChange) => {
+  const options = [
+    { v: '', t: 'Todos' },
+    { v: 'action', t: 'Ação' },
+    { v: 'comedy', t: 'Comédia' },
+    { v: 'thriller', t: 'Suspense' },
+  ];
+  return (
+    <label htmlFor={id}>
+      {label}
+      <select value={value} onChange={onChange}>
+        {options.map((o, i) => (
+          <option key={i} value={o.v}>
+            {o.t}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+};
+
+const SearchBar = props => {
   const {
     searchText,
     onSearchTextChange,
@@ -13,33 +45,28 @@ const SearchBar = (props) => {
 
   return (
     <form>
-      <label htmlFor="search-bar-input">
-        Inclui o texto:
-        <input
-          id="search-bar-input"
-          type="text"
-          value={searchText}
-          onChange={onSearchTextChange}
-        />
-      </label>
-      <label htmlFor="search-bar-checkbox">
-        Mostrar somente favoritos
-        <input
-          id="search-bar-checkbox"
-          type="checkbox"
-          checked={bookmarkedOnly}
-          onChange={onBookmarkedChange}
-        />
-      </label>
-      <label htmlFor="search-bar-select">
-        Filtrar por gênero
-        <select value={selectedGenre} onChange={onSelectedGenreChange}>
-          <option value="">Todos</option>
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
-      </label>
+      {createInputField(
+        'search-bar-input',
+        'Inclui o texto:',
+        'text',
+        searchText,
+        false,
+        onSearchTextChange,
+      )}
+      {createInputField(
+        'search-bar-checkbox',
+        'Mostrar somente favoritos',
+        'checkbox',
+        false,
+        bookmarkedOnly,
+        onBookmarkedChange,
+      )}
+      {createSelectField(
+        'search-bar-select',
+        'Filtrar por gênero',
+        selectedGenre,
+        onSelectedGenreChange,
+      )}
     </form>
   );
 };
