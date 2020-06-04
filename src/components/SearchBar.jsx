d50e030dd996145113668b372ece5a76889ced33
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-04 14:51:30 -0300

    Vamos que vamos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { serachText, onSearchTextChange,
+      BookmarkedChange, selectedGenre,
+      onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
