a9dd16eaea2943365857a38aa11aedaca68a62db
Diego Campos <diegodiko05@gmail.com>
2020-06-04 16:03:49 -0300

    CC 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,40 +4,21 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange, } = this.props;
     return (
       <form>
         <label htmlFor="searchText">
           Inclui o texto:
-          <input
-            name="searchText"
-            type="text"
-            value={searchText}
-            onChange={onSearchTextChange}
-          />
+          <input name="searchText" type="text" value={searchText} onChange={onSearchTextChange} />
         </label>
-        <label htmlFor="favorites">
-          Mostrar somente favoritos
-          <input
-            name="favorites"
-            type="checkbox"
+        <label htmlFor="favorites">Mostrar somente favoritos
+          <input name="favorites" type="checkbox"
             checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
-          />
+            onChange={onBookmarkedChange} />
         </label>
-        <label htmlFor="genreFilter">
-          Filtrar por gênero
-          <select
-            name="genreFilter"
-            value={selectedGenre}
-            onChange={onSelectedGenreChange}
-          >
+        <label htmlFor="genreFilter">Filtrar por gênero
+          <select name="genreFilter" value={selectedGenre} onChange={onSelectedGenreChange}>
             <options value="">Todos</options>
             <options value="action">Ação</options>
             <options value="comedy">Comédia</options>
