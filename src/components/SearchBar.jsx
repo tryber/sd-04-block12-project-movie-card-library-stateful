8e8f01edaa991ffebbe9fce9eaeb64f7546edf17
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-04 15:53:34 -0300

    Adicionadas props para o componente SearchBar.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <h1>Texto de exemplo</h1>
+    );
+  }
+}
+
+export default SearchBar;
