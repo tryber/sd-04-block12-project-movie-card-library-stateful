96c5c5cebe9a393153b70b65dc02053be9cf79f3
cacobribeiro <cacobribeiro@gmail.com>
2020-06-04 15:38:54 -0300

    Implementado SeachBar inicial

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,37 @@
-// implement SearchBar component here
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: 'all',
+      bookmarkedOnlt: false,
+      selectedGenre: '',
+    }
+  }
+
+  onSearchTextChange = event => {
+    this.setState({
+      filter: event.target.value === '' ? 'all' : event.target.value
+    })
+  }
+
+  onBookmarkedChange = event => {
+
+  }
+
+  onSelectedGenreChange = event => {
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <form>
+          <input type='text' />
+        </form>
+      </div>
+    );
+  }
+}
