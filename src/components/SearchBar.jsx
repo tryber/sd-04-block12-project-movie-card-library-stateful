bac0946dfc00b2cfd8daec3e01f8a25fba6cf599
orlando-messias <programadorlando@gmail.com>
2020-06-04 18:33:37 -0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,15 +5,15 @@ export default class MovieLibrary extends React.Component {
   render() {
     return (
       <div>
-          <span>Inclui o texto:</span>
-          <input
-            type="text" name="search" value={this.props.searchText}
-            onChange={this.props.onSearchTextChange}
-          />
-          <br />
+        <span>Inclui o texto:</span>
         <input
-          type="checkbox" name="bookmark" checked={this.props.bookmarkedOnly}
-          onChange={this.props.onBookmarkedChange}
+          type="text" name="search" value={this.props.searchText}
+          onChange={this.props.onSearchTextChange}
+        />
+        <br />
+        <input
+        type="checkbox" name="bookmark" checked={this.props.bookmarkedOnly}
+        onChange={this.props.onBookmarkedChange}
         />
         <span>Mostrar somente favoritos</span>
         <br />
