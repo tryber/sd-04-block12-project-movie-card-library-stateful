1537fb35382c42d0bc8bd9d9bed28d0c851e6bee
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-04 15:22:52 -0300

    subindo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,26 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subtitle: '', title: '',
+      imagePath: '', storyline: '',
+      rating: 0, genre: 'action',
+    }
+  }
+
+  render() {
+    cosnt { onClick } = this.props;
+    return (
+      <div>
+        <form>
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
