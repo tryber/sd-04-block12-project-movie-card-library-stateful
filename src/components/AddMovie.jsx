22ed57b9f8e23e537cb9e7268f7b9508e7fd0ef7
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-04 15:54:25 -0300

    cansando ja

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       subtitle: '', title: '',
@@ -11,12 +11,41 @@ class AddMovie extends Component {
     }
   }
 
+  ChangeState(event) {
+    console.log(event.target)
+    const { name, value} = event.target;
+    this.setState(() => {
+      [name]: value;
+    })
+  }
+
+  AddMovie() {
+    console.log(this.state)
+    this.props.onClick(this.state)
+  }
+
   render() {
-    cosnt { onClick } = this.props;
+    const { onClick } = this.props;
     return (
       <div>
-        <form>
-
+        <form action='' method='post' enctype="multipart/form-data">
+          <label id='inputOne'>Título</label>
+          <input id='inputOne' type='text' name='title' value='this.state.title' onChange={this.ChangeState} />
+          <label id='inputTwo'>Subtítulo</label>
+          <input id='inputTwo' type='text' name='subtitle' value='this.state.subtitle' onChange={this.ChangeState} />
+          <label id='inputThree'>Imagem</label>
+          <input id='inputThree' type='text' name='imagePath' value='this.state.imagePath' onChange={this.ChangeState} />
+          <label id='inputFour'>Sinopse</label>
+          <textarea id='inputFour' type='text' name='storyline' value='this.state.storyline' onChange={this.ChangeState} />
+          <label id='inputFive'>Avaliação</label>
+          <input id='inputFive' type='number' name='rating' value='this.state.rating' onChange={this.ChangeState} />
+          <label id='inputSix'>Gênero</label>
+          <select id='inputSix' name='genre' value={this.state.genre} onChange={this.ChangeState}>
+          <option value='action'>Ação</option>
+          <option value='comedy'>Comédia</option>
+          <option value='thriller'>Suspense</option>
+        </select>
+        <button onChange={this.AddMovie}>Adicionar Filme</button>
         </form>
       </div>
     );
