ae8a37ab65200f55330ebe2a5cc33cf813ff4193
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-05 01:41:35 -0300

    AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,71 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }, name) {
+    const e = (name === 'rating') ? Number(value) : value;
+    this.setState({ [name]: e });
+  }
+
+  createElement(type, name, text, value, callback = this.handleChange) {
+    return (
+      <label htmlFor={name}>
+        {text}
+        <input type={type} name={name} value={value} onChange={(event) => callback(event, name)} />
+      </label>
+    );
+  }
+
+  reset(onClick) {
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    const { onClick } = this.props;
+    return (
+      <form>
+        {this.createElement('text', 'title', 'Título', title)}
+        {this.createElement('text', 'subtitle', 'Subtítulo', subtitle)}
+        {this.createElement('text', 'imagePath', 'Imagem', imagePath)}
+        <label htmlFor>
+          Sinopse
+          <textarea value={storyline} onChange={(event) => this.handleChange(event, 'storyline')} />
+        </label>
+        {this.createElement('number', 'rating', 'Avaliação', rating)}
+        <label htmlFor="genre">
+          Gênero
+          <select name="genre" value={genre} onChange={(event) => this.handleChange(event, 'genre')}>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+          <button type="button" onClick={() => this.reset(onClick)}>Adicionar filme</button>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
