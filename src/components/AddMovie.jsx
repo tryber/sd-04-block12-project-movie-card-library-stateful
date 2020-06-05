6d59b2682e0ea2432ed89306f696fc2f32e53b21
Diego Campos <diegodiko05@gmail.com>
2020-06-05 09:56:44 -0300

    Agora vai?

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -27,9 +27,9 @@ class AddMovie extends React.Component {
   }
 
   hand(event, name) {
-    return (name === 'rating') ? 
-    this.setState({ [name]: Number(event.target.value) })
-    : this.setState({ [name]: event.target.value });
+    return name === 'rating'
+      ? this.setState({ [name]: Number(event.target.value) })
+      : this.setState({ [name]: event.target.value });
   }
 
   includeMovie() {
@@ -42,26 +42,65 @@ class AddMovie extends React.Component {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form>
-        <label htmlFor="title">Título
-          <input name="title" type="text" value={title} onChange={(e) => this.hand(e, 'title')} /></label>
-        <label htmlFor="subtitle">Subtítulo
-          <input name="subtitle" type="text" value={subtitle} onChange={(e) => this.hand(e, 'subtitle')} /></label>
-        <label htmlFor="imagePath">Imagem
-          <input name="imagePath" type="text" value={imagePath} onChange={(e) => this.hand(e, 'imagePath')} />
+        <label htmlFor="title">
+          Título
+          <input
+            name="title"
+            type="text"
+            value={title}
+            onChange={(e) => this.hand(e, 'title')}
+          />
         </label>
-        <label htmlFor="storyline">Sinopse
-          <textarea name="storyline" value={storyline} onChange={(e) => this.hand(e, 'storyline')} /></label>
-        <label htmlFor="rating">Avaliação
-          <input name="rating" type="number" value={rating} onChange={(e) => this.hand(e, 'rating')} />
+        <label htmlFor="subtitle">
+          Subtítulo
+          <input
+            name="subtitle"
+            type="text"
+            value={subtitle}
+            onChange={(e) => this.hand(e, 'subtitle')}
+          />
         </label>
-        <label htmlFor="genre">Gênero
-          <select name="genre" value={genre} onChange={(e) => this.hand(e, 'genre')}>
+        <label htmlFor="imagePath">
+          Imagem
+          <input
+            name="imagePath"
+            type="text"
+            value={imagePath}
+            onChange={(e) => this.hand(e, 'imagePath')}
+          />
+        </label>
+        <label htmlFor="storyline">
+          Sinopse
+          <textarea
+            name="storyline"
+            value={storyline}
+            onChange={(e) => this.hand(e, 'storyline')}
+          />
+        </label>
+        <label htmlFor="rating">
+          Avaliação
+          <input
+            name="rating"
+            type="number"
+            value={rating}
+            onChange={(e) => this.hand(e, 'rating')}
+          />
+        </label>
+        <label htmlFor="genre">
+          Gênero
+          <select
+            name="genre"
+            value={genre}
+            onChange={(e) => this.hand(e, 'genre')}
+          >
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
             <option value="thriller">Suspense</option>
           </select>
         </label>
-        <button type="button" onClick={this.includeMovie}>Adicionar filme</button>
+        <button type="button" onClick={this.includeMovie}>
+          Adicionar filme
+        </button>
       </form>
     );
   }
