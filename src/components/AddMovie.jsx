9911e6b7ec8362bc8be1245434574eaedb050e45
djonata-dias <diasdjonata@gmail.com>
2020-06-04 21:28:51 -0300

    terminado o  addMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
+function Input(props) {
+  return (
+    <label htmlFor={props.name}>{props.label}
+      <Input name={props.name} type={props.type} onChange={props.onchange} value={props.value} />
+    </label>
+  );
+}
+
+function Select(props) {
+  return (
+    <label htmlFor="genre">Gênero
+      <select name="genre" type="select" value={props.value} onChange={props.onchange}>
+        <option value="">Todos</option>
+        <option value="action">Ação</option>
+        <option value="comedy">Comédia</option>
+        <option value="thriller">Suspense</option>
+      </select>
+    </label>
+  );
+}
+
 export default class AddMovie extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({
+    this.state = ({ subtitle: '', title: '', imagePath: '', storyLine: '', rating: 0, genre: 'action' });
+    this.onClick = this.onClick.bind(this);
+    this.changeState = this.changeState.bind(this);
+  }
+
+  onClick(event) {
+    this.setState({
       subtitle: '',
       title: '',
       imagePath: '',
@@ -11,48 +38,23 @@ export default class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     });
-    this.changeStates = this.changeState.bind(this);
-    this.onClick = this.onClick.bind(this);
+    event.preventDefault();
   }
-    changeStates = event => {
-      const target = event.target;
-      this.setState({[target.name]: target.value})
-    };
-    onClick = event => {
-      this.setState({
-        subtitle: '',
-        title: '',
-        imagePath: '',
-        storyLine: '',
-        rating: 0,
-        genre: 'action',
-      });
-      event.preventDefault();
-    };
-    render() {
+
+  changeState(event) {
+    const { target } = event;
+    this.setState({ ...this.state, [target.name]: target.value });
+  }
+
+  render() {
     return (
       <form onSubmit={this.onClick}>
-        <label htmlFor="title">Título<input name="title" type="text" value={this.state.title} />
-        </label>
-        <label htmlFor="subtitle">Subtítulo
-          <input name="subtitle" type="text" value={this.state.subtitle} />
-        </label>
-        <label htmlFor="imagePath">Imagem
-          <input name="imagePath" type="text" value={this.state.imagePath} />
-        </label>
-        <label htmlFor="storyLine">Sinopse
-          <input name="storyLine" type="textarea" value={this.state.storyLine} />
-        </label>
-        <label htmlFor="rating">Avaliação<input name="rating" type="text" value={this.state.rating} /></label>
-        <label htmlFor="genre">Gênero
-         <select name="genre" type="select" value={this.state.genre}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
-        <input type="submit" value="Adicionar filme" onClick={this.onClick} />
+        <Input name={'title'} type={'text'} onchange={this.changeState} value={this.state.title} label={'Título'} />
+        <Input name={'subtitle'} type={'text'} onchange={this.changeState} value={this.state.subtitle} label={'Subtítulo'} />
+        <Input name={'imagePath'} type={'text'} onchange={this.changeState} value={this.state.imagePath} label={'Imagem'} />
+        <Input name={'storyLine'} type={'textarea'} onchange={this.changeState} value={this.state.storyLine} label={'Sinopse'} />
+        <Input name={'rating'} type={'text'} onchange={this.changeState} value={this.state.rating} label={'Avaliação'} />
+        <Select onchange={this.changeState} value={this.state.genre} />
       </form>
     );
   }
