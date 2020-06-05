a67297530a01f9d84729c0e61b8c3ad83ec1e90a
deboracosilveira <deboracosilveira@gmail.com>
2020-06-05 12:03:09 -0300

    SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,50 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: movies,
+    };
+  }
+
+  onSearchTextChange() {}
+
+  onBookmarkedChange() {}
+
+  onSelectedGenreChange() {}
+
+  render() {
+    const {
+      searchText,
+      bookmarkedOnly,
+      selectedGenre,
+      movies: { movies },
+    } = this.state;
+
+    return (
+      <div>
+        <SearchBar
+          searchText={searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
