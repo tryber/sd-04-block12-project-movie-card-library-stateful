54f8cf7898912ba5cf0d625c461e6827f2794901
igorgbr <igorgphotoarte@gmail.com>
2020-06-05 02:23:52 -0300

    testando CC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import MovieLibrary from './components/MovieLibrary';
+import SearchBar from './components/SearchBar';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <MovieLibrary />
+
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,36 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      image, onBookmarkedChange, selectedGenre, onSelectedGenreChange
+    } = this.props;
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Inclui o texto: </label>
+            <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+            <br />
+            <label>Mostrar somente favoritos &nbsp;</label>
+            <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}/>
+            <br />
+            <label>Filtrar por gênero</label>
+            <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+            </select>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
