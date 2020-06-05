3f2ad764c3e00ac5bad440e3137e8d34a5b387b8
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-05 15:53:58 -0300

    da pra liberar porra

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,26 @@
 // implement SearchBar component here
 import React, { Component } from 'react';
 
+function inputs({ name, value, onChange, type, label }) {
+  return (
+    <div>
+      <label htmlFor={name}>
+        {label}
+        <input type={type} name={name} value={value} onChange={onChange} />
+      </label>
+    </div>
+  );
+}
+
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, onBookmarkedChange, selectedGenre, onSelectedGenreChange, bookmarkedOnly } = this.props;
+    const { searchText, onSearchTextChange,
+      onBookmarkedChange, selectedGenre,
+      onSelectedGenreChange, bookmarkedOnly } = this.props;
     return (
       <form>
-        <label htmlFor="search">
-          Inclui o texto:
-          <input type="text" name="search" value={searchText} onChange={onSearchTextChange} />
-        </label>
-        <label htmlFor="checkbox">
-          Mostrar somente favoritos
-          <input type="checkbox" name="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-        </label>
+        {inputs({ name: 'search', label: 'Inclui o texto:', type: 'text', value: searchText, onChange: onSearchTextChange })}
+        {inputs({ name: 'checkbox', label: 'Mostrar somente favoritos', type: 'checkbox', value: bookmarkedOnly, onChange: onBookmarkedChange })}
         <label htmlFor="genre">
           Filtrar por gênero
           <select value={selectedGenre} name="genre" onChange={onSelectedGenreChange}>
