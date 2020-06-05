c6a22f58544718ad075efef44b1019bb3017df98
Victor L <victor.lustosacp@gmail.com>
2020-06-05 16:59:08 -0300

    atualizando

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,25 +9,25 @@ class SearchBar extends Components {
       bookmarkedOnly,
       onBookmakedChange,
       selectGenre,
-      onSelectGenreChange
+      onSelectGenreChange,
     } = this.props;
 
-  return (
-    <form>
-      <label for="nomeF">Inclue o Texto</label>
-      <input id="nomeF"type="text" value={searchText} onChange={onSearchTextChange}></input>
-      <label for="check">Mostrar somente Favoritos</label>
-      <input id="check" type="checkbox" checked={bookmarkedOnly}
-        onChange={onBookmakedChange}></input>
-      <label id="gender">Filtrar por Gênero</label>
-      <select id="gender" value={selectGenre} onChange={onSelectGenreChange}>
-        <option key="Todos" value="">Todos</option>
-        <option key="Ação" value="action">Ação</option>
-        <option key="Comédia" value="comedy">Comédia</option>
-        <option key="Suspense" value="thiller">Suspense</option>
-      </select>
-    </form>
-  )
+    return (
+      <form>
+        <label for="nomeF">Inclue o Texto</label>
+        <input id="nomeF"type="text" value={searchText} onChange={onSearchTextChange}></input>
+        <label for="check">Mostrar somente Favoritos</label>
+        <input id="check" type="checkbox" checked={bookmarkedOnly}
+          onChange={onBookmakedChange}></input>
+        <label id="gender">Filtrar por Gênero</label>
+        <select id="gender" value={selectGenre} onChange={onSelectGenreChange}>
+          <option key="Todos" value="">Todos</option>
+          <option key="Ação" value="action">Ação</option>
+          <option key="Comédia" value="comedy">Comédia</option>
+          <option key="Suspense" value="thiller">Suspense</option>
+        </select>
+      </form>
+    )
   }
 }
 
