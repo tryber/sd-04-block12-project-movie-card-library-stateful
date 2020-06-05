5be582130253b326c32ef169c23c7d4556a734b2
orlando-messias <programadorlando@gmail.com>
2020-06-04 21:33:09 -0300

    MovieList and function onSearchTextChange

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,6 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-
     return (
       <div className="movie-list">
         {this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
