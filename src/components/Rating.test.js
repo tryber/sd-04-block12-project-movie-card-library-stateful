a2da8c44b29bef90935ebd279d4a4b017bde0bd7
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-05 17:52:00 -0300

    Resolvendo conflitos.

--- src/components/Rating.test.js
+++ src/components/Rating.test.js
@@ -4,11 +4,11 @@ import { shallow } from 'enzyme';
 import Rating from './Rating';
 
 describe('<Rating /> component', () => {
-  it.skip('renders without crashing', () => {
+  it('renders without crashing', () => {
     shallow(<Rating />);
   });
 
-  it.skip('renders the rating inside an element with the class `rating`', () => {
+  it('renders the rating inside an element with the class `rating`', () => {
     const wrapper = shallow(<Rating rating={3} />);
 
     expect(wrapper.find('.rating').text()).toEqual('3');
