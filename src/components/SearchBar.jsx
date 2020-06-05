7c1635b2e8ef75d83754d36645c91afb84c20fd7
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-06-04 23:42:00 -0300

    cade meu review

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,27 +34,6 @@ class SearchBar extends React.Component {
     );
   }
 
-  // formSelectElements() {
-  //   const { selectedGenre, onSelectedGenreChange } = this.props;
-
-  //   return (
-  //     <label htmlFor="genre">
-  //       Filtrar por gênero
-  //       <select
-  //         name="selectedGenre"
-  //         id="genre"
-  //         value={selectedGenre}
-  //         onChange={onSelectedGenreChange}
-  //       >
-  //         <option value="">Todos</option>
-  //         <option value="action">Ação</option>
-  //         <option value="comedy">Comédia</option>
-  //         <option value="thriller">Suspense</option>
-  //       </select>
-  //     </label>
-  //   );
-  // }
-
   render() {
     const { selectedGenre, onSelectedGenreChange } = this.props;
     return (
