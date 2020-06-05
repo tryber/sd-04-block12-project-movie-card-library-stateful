392f28b73fbb182aabc2e02d64977a6a5c6320de
Diego Campos <diegodiko05@gmail.com>
2020-06-05 09:49:47 -0300

    Esta passando nos teste, funcionando beleza, o CC vai deixar??

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -26,8 +26,10 @@ class AddMovie extends React.Component {
     });
   }
 
-  hand(event) {
-    this.setState({ [event.target.name]: event.target.value });
+  hand(event, name) {
+    name === 'rating' ? 
+    this.setState({ [name]: Number(event.target.value) })
+    : this.setState({ [name]: event.target.value });
   }
 
   includeMovie() {
@@ -41,19 +43,19 @@ class AddMovie extends React.Component {
     return (
       <form>
         <label htmlFor="title">Título
-          <input name="title" type="text" value={title} onChange={this.hand} /></label>
+          <input name="title" type="text" value={title} onChange={(e) => this.hand(e, 'title')} /></label>
         <label htmlFor="subtitle">Subtítulo
-          <input name="subtitle" type="text" value={subtitle} onChange={this.hand} /></label>
+          <input name="subtitle" type="text" value={subtitle} onChange={(e) => this.hand(e, 'subtitle')} /></label>
         <label htmlFor="imagePath">Imagem
-          <input name="imagePath" type="text" value={imagePath} onChange={this.hand} />
+          <input name="imagePath" type="text" value={imagePath} onChange={(e) => this.hand(e, 'imagePath')} />
         </label>
         <label htmlFor="storyline">Sinopse
-          <textarea name="storyline" value={storyline} onChange={this.hand} /></label>
+          <textarea name="storyline" value={storyline} onChange={(e) => this.hand(e, 'storyline')} /></label>
         <label htmlFor="rating">Avaliação
-          <input name="rating" type="number" value={rating} onChange={this.hand} />
+          <input name="rating" type="number" value={rating} onChange={(e) => this.hand(e, 'rating')} />
         </label>
         <label htmlFor="genre">Gênero
-          <select name="genre" value={genre} onChange={this.hand}>
+          <select name="genre" value={genre} onChange={(e) => this.hand(e, 'genre')}>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
             <option value="thriller">Suspense</option>
