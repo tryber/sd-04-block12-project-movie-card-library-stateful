ca5bcb31be800fbce362dd39f11d105cecd247b3
cacobribeiro <cacobribeiro@gmail.com>
2020-06-05 17:17:07 -0300

    Test 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -29,13 +29,12 @@ export default class SearchBar extends React.Component {
           </label>
           <label htmlFor="favoritos">Mostrar somente favoritos <input
             type="checkbox"
-            checked={this.props.bookmarkedOnly}
-            onChange={this.props.onBookmarkedChange}
+            checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}
           />
           </label>
-          <label htmlFor="genre">Filtrar por gênero<select
-            value={this.props.selectedGenre}
-            onChange={this.props.onSelectedGenreChange}
+          <label htmlFor="genre">Filtrar por gênero
+          <select
+            value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}
           >
             <option value="">Todos</option>
             <option value="action">Ação</option>
