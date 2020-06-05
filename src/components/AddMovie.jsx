f994969ea3a294739ce1736cc65d75544d871ad2
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-05 15:36:01 -0300

    fix cc

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,8 @@
 // import React, { Component } from 'react';
-
 // class AddMovie extends Component {
 //   render() {
 //     const { title, subtitle, storyline, imagePath, rating ,genre } = this.props;
-//     return 
+//     return
 //   }
 // }
     
