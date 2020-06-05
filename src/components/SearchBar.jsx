b2905ef938dd6d7eb6a962c5d0f52e12fbaddfc4
Caio Magalhães <caio.lmf@gmail.com>
2020-06-04 23:03:54 -0300

    Implemets SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,44 @@
-// implement SearchBar component here
+import React, { PureComponent } from 'react';
+
+class SearchBar extends PureComponent {
+  genres = {
+    Todos: '',
+    Ação: 'action',
+    Comédia: 'comedy',
+    Suspense: 'thriller',
+  };
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <form>
+        <label>Inclui o texto</label>
+        <input type="text" value={searchText} onChange={onSearchTextChange} />
+        <label>Mostrar somente favoritos</label>
+        <input
+          type="checkbox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+        />
+        <label>Filtrar por gênero</label>
+        <select value={selectedGenre} onChange={onSelectedGenreChange}>
+          {Object.keys(this.genres).map((genre) => (
+            <option key={this.genres[genre]} value={this.genres[genre]}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
