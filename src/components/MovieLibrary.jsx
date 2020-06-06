f20f7eba2e400bc5f07a6cdc1f73700b427a17d2
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-05 23:54:16 -0300

    updating Movie Library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,1 @@
-import React, { Component } from 'react';
-
-class MovieLibrary extends Component {
-  render() {
-      const [] = this.props;
-    return;
-  }
-}
-
-export default MovieLibrary;
+// import React from 'react';
