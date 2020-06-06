7d233b2b0729119c53eea99c3ec81f11a571e1a8
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-05 23:46:50 -0300

    initicatemovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,10 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+      const [] = this.props;
+    return;
+  }
+}
+
+export default MovieLibrary;
