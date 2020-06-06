98330f3f1383b54f077306fc1876c15bfce60e19
igorgbr <igorgphotoarte@gmail.com>
2020-06-06 20:24:22 -0300

    alteraçoes hards no CC 7

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,34 +2,40 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   const {
-  //     searchText, onSearchTextChange, bookmarkedOnly,
-  //     image, onBookmarkedChange, selectedGenre, onSelectedGenreChange
-  //   } = this.props;
-  // }
+  constructor(props) {
+    super(props);
+    this.renderHalf = this.renderHalf.bind(this);
+  }
+
+  renderHalf() {
+    return (
+      <div>
+        {' '}
+        <label htmlFor="searchText">Inclui o texto: </label>
+        <input
+          name="searchText"
+          type="text"
+          value={this.props.searchText}
+          onChange={this.props.onSearchTextChange}
+        />
+        <br />
+        <label htmlFor="bookmarkedOnly">Mostrar somente favoritos &nbsp;</label>
+        <input
+          name="bookmarkedOnly"
+          type="checkbox"
+          checked={this.props.bookmarkedOnly}
+          onChange={this.props.onBookmarkedChange}
+        />
+        <br />
+      </div>
+    );
+  }
 
   render() {
     return (
       <div>
         <form>
-          <label htmlFor="searchText">Inclui o texto: </label>
-          <input
-            name="searchText"
-            type="text"
-            value={this.props.searchText}
-            onChange={this.props.onSearchTextChange}
-          />
-          <br />
-          <label htmlFor="bookmarkedOnly">Mostrar somente favoritos &nbsp;</label>
-          <input
-            name="bookmarkedOnly"
-            type="checkbox"
-            checked={this.props.bookmarkedOnly}
-            onChange={this.props.onBookmarkedChange}
-          />{' '}
-          <br />
+          {this.renderHalf()}
           <label htmlFor="selectedGenre">Filtrar por gênero</label>
           <select
             name="selectedGenre"
