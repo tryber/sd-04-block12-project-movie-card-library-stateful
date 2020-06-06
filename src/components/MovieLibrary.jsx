c8aff4e05e726664feefd231b183e8d1d56549e2
binhus <eng.robsonwilli@gmail.com>
2020-06-05 22:52:40 -0300

    SearchBar feature

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
 // implement MovieLibrary component here
+import React from 'react';
+import Searchbar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+export default class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: '',
+    };
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <Searchbar
+          searchText={this.state.searchText}
+          onSearchTextChange={(event) =>
+            this.setState({ searchText: event.target.value })
+          }
+          onBookmarkedChange=""
+          bookmarkedOnly=""
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange=""
+        />
+        <MovieList movies={movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,54 @@
-// implement SearchBar component here
+import React from 'react';
+
+export default class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      onBookmarkedChange,
+      bookmarkedOnly,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <div>
+        <form action="">
+          <label htmlFor="includesText">Inclui o texto</label>
+          <br />
+          <input
+            onChange={onSearchTextChange}
+            type="text"
+            name="includesText"
+            value={searchText}
+          />
+          <br />
+          <input
+            onChange={onBookmarkedChange}
+            type="checkbox"
+            name="checkbox"
+            checked={bookmarkedOnly}
+          />
+          <label htmlFor="checkbox">Mostrar somente favoritos</label>
+          <br />
+          <label htmlFor="select">Filtrar por gênero</label>
+          <br />
+          <select
+            onChange={onSelectedGenreChange}
+            name="select  "
+            id=""
+            value={selectedGenre}
+          >
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </form>
+      </div>
+    );
+  }
+}
