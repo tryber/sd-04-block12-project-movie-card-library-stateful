d7397d8423a1ba4b4c7d39d4160026a472bf5847
igorgbr <igorgphotoarte@gmail.com>
2020-06-06 20:10:19 -0300

    alteraçoes hards no CC 6

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,12 +15,27 @@ class SearchBar extends Component {
       <div>
         <form>
           <label htmlFor="searchText">Inclui o texto: </label>
-          <input name="searchText" type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+          <input
+            name="searchText"
+            type="text"
+            value={this.props.searchText}
+            onChange={this.props.onSearchTextChange}
+          />
           <br />
           <label htmlFor="bookmarkedOnly">Mostrar somente favoritos &nbsp;</label>
-          <input name="bookmarkedOnly" type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} /> <br />
+          <input
+            name="bookmarkedOnly"
+            type="checkbox"
+            checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange}
+          />{' '}
+          <br />
           <label htmlFor="selectedGenre">Filtrar por gênero</label>
-          <select name="selectedGenre" value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+          <select
+            name="selectedGenre"
+            value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange}
+          >
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
