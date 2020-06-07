d6b3a24c1efcffcdd3abd7114b39e2d9c8f7c9af
Julio Cesar <stininmatic@hotmail.com>
2020-06-07 19:19:50 -0300

    Testando EsLint novamente

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,46 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        {' '}
+        {/* CRIANDO O FORMS */}
+        <form>
+          <label name="entrada">Inclui o texto: </label>
+          <input
+            type="text"
+            name="entrada"
+            value={this.props.searchText}
+            onChange={this.props.onSearchTextChange}
+          />
+          <br />
+          <label name="check">Mostrar somente favoritos</label>
+          <input
+            type="checkbox"
+            name="check"
+            checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange}
+          />
+          <br />
+          <label name="selecao">Filtrar por gênero</label>
+          <select
+            name="selecao"
+            value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange}
+          >
+            <br />
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+          <br />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
