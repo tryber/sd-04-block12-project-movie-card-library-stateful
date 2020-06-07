6092e13041c70291255ca02402891034ca388979
fogo5000 <lucaslucas2008@gmail.com>
2020-06-07 02:39:14 -0300

    Pass tests 1-4 1.0v

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,27 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+
+export default class SearchBar extends Component {
+
+    searchText = event => {
+        console.log();
+    };
+    
+    onSearchTextChange = event => {
+        console.log();
+    };
+    
+    render () {
+        return (
+            <div>
+                <form>
+                    <label>
+                        Inclui o texto:
+                        <input type="text" value={this.searchText} onChange={this.onSearchTextChange}></input>
+                    </label>
+                </form>
+            </div>
+        );
+    }
+
+}
