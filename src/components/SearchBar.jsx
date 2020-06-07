c4e3fa516b8bfad88a17d745eecddba0486b80ba
wistyk <leonardoxw@gmail.com>
2020-06-07 14:11:54 -0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <label>Inclui o texto</label>
+        <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+        <label>Mostrar somente favoritos</label>
+        <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+        <label>Filtrar por gênero</label>
+        <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange} />
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
