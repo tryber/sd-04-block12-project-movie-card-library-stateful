4d1f5d5be111597dc6bbd21a30760f72613b364a
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-06-07 02:05:31 -0300

    Function registerValidSW has 31 lines of code (exceeds 25 allowed).

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -64,13 +64,8 @@ function registerValidSW(swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-              );
-              if (config && config.onUpdate) {
-                config.onUpdate(registration);
-              }
+              console.log('New content is available and will be used when all tabs for this page are closed. See https://bit.ly/CRA-PWA.');
+              if (config && config.onUpdate) config.onUpdate(registration);
             } else {
               console.log('Content is cached for offline use.');
               if (config && config.onSuccess) config.onSuccess(registration);
