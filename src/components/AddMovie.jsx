cf797ae94e2d6a833c64ebe99d100d2d7db0950a
wistyk <leonardoxw@gmail.com>
2020-06-07 15:26:40 -0300

    addMovies

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,18 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    const { title, onClick } = this.props;
+    return (
+      <form>
+        <label htmlFor="Título">
+          Título
+          <input type="text" value={title} onChange={onClick} />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
