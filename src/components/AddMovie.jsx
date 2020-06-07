6a1d9f151ba28e0ac87db7169ee45c5e86b9fb55
wistyk <leonardoxw@gmail.com>
2020-06-07 16:36:36 -0300

    addMovie com pendências

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,13 +3,36 @@ import React, { Component } from 'react';
 
 class AddMovie extends Component {
   render() {
-    const { title, onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating, genre, onClick } = this.props;
     return (
       <form>
         <label htmlFor="Título">
           Título
-          <input type="text" value={title} onChange={onClick} />
+          <input type="text" value={title} />
         </label>
+        <label htmlFor="Subtítulo">
+          Subtítulo
+          <input type="text" value={subtitle} />
+        </label>
+        <label htmlFor="Imagem">
+          Imagem
+          <input type="text" value={imagePath} />
+        </label>
+        <label htmlFor="Sinopse">
+          Sinopse
+          <textarea value={storyline} cols="30" rows="10" />
+        </label>
+        <label htmlFor="Avaliação">
+          Avaliação
+          <input type="number" value={rating} />
+        </label>
+        <label htmlFor="Gênero">Gênero</label>
+        <select value={genre}>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspence</option>
+        </select>
+        <button>Adicionar filme</button>
       </form>
     );
   }
