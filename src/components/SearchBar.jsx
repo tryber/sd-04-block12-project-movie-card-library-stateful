c721595be4d21826dd509319663e4ea271f95a08
Julianafatsil <julianafatsil@gmail.com>
2020-06-07 15:17:41 -0300

    Revision Issues

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,9 +9,9 @@ class SearchBar extends Component {
     // inicializo stado das variaveis que sofrerão alteração
     this.state = {
       // objeto de ações
-      searchText: "",
+      searchText: '',
       bookmarkedOnly: false,
-      selectedGenre: "",
+      selectedGenre: '',
     };
     // uso bind quando tenho alteração no meu state / controlador
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
@@ -21,22 +21,16 @@ class SearchBar extends Component {
 
   onSearchTextChange(event) {
     const { value } = event.target;
-    this.setState({
-      searchText: value, // recebe o novo value
-    });
+    this.setState({ searchText: value, }); // recebe o novo value
   }
 
   onBookmarkedChange() {
-    this.setState({
-      bookmarkedOnly: !this.state.bookmarkedOnly, // nega o que vem
-    });
+    this.setState({ bookmarkedOnly: !this.state.bookmarkedOnly, });// nega o que vem
   }
 
   onSelectedGenreChange(event) {
     const { value } = event.target; // pegar valor do campo
-    this.setState({
-      selectedGenre: value, // selectedGenre vale oque usuario seleciona
-    });
+    this.setState({ selectedGenre: value, }); // selectedGenre vale oque usuario seleciona
   }
 
   render() {
