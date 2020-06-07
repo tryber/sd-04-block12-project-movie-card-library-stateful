268b31b43c0d6c12b8409b7459629d52406c99bb
fogo5000 <lucaslucas2008@gmail.com>
2020-06-07 19:26:53 -0300

    Pass test 5/6 searchBar CC fix 1.0v

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 
 
-export default class SearchBar extends Component {    
-    render () {
-        const { searchText, onSearchTextChange } = this.props;
-        return (
-            <div>
-                <form>
-                    <label>
-                        Inclui o texto:
-                        <input type="text" value={searchText} onChange={onSearchTextChange}></input>
-                    </label>
-                </form>
-            </div>
-        );
-    }
-
+export default class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <div>
+        <form>
+          <label>
+            Inclui o texto:
+            <input type="text" value={searchText} onChange={onSearchTextChange} />
+          </label>
+        </form>
+      </div>
+    );
+  }
 }
