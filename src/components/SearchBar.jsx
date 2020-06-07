c3567ab0302e56202af47dd84163ac5b787c1817
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-06-07 03:21:23 -0300

    agora vai

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,12 +2,11 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  
   render() {
     const {
       searchText, onSearchTextChange,
       bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange
+      selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
       <form>
