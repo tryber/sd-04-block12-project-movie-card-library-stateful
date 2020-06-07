8551f88b554c00a0191177566bb74f8249004917
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-06-07 01:06:34 -0300

    Requisito Search Bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import data from './data';
+import SearchBar from './components/SearchBar'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={data} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,45 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange,
+      } = this.props;
+    return (
+      <form>
+        <label>
+          Inclui o texto
+          <input
+            type="text"
+            name="searchText"
+            value={searchText}
+            onChange={onSearchTextChange}
+            placeholder="Procurar"
+          />
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="bookMarkedOnly"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          /> Mostrar somente favoritos
+        </label>
+        <label>
+          Filtrar por gênero
+          <select name="select" value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
