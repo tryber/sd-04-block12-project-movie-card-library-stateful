2c79644aa51a30874af2053547530fdaa981d12d
Julio Cesar <stininmatic@hotmail.com>
2020-06-07 20:24:55 -0300

    Testando EsLint novamente

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -40,14 +40,37 @@ class AddMovie extends React.Component {
     return (
       <form>
         <label htmlFor="title">Título</label>
-        <input type="text" name="title" id="title" value={this.state.title} onChange={this.handleInputChange} />
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={this.state.title}
+          onChange={this.handleInputChange}
+        />
         <label htmlFor="subtitle">Subtítulo</label>
-        <input type="text" name="subtitle" id="subtitle" value={this.state.subtitle} onChange={this.handleInputChange} />
+        <input
+          type="text"
+          name="subtitle"
+          id="subtitle"
+          value={this.state.subtitle}
+          onChange={this.handleInputChange}
+        />
         <label htmlFor="image">Imagem</label>
-        <input type="text" name="imagePath" id="image" value={this.state.imagePath} onChange={this.handleInputChange} />
+        <input
+          type="text"
+          name="imagePath"
+          id="image"
+          value={this.state.imagePath}
+          onChange={this.handleInputChange}
+        />
         <label htmlFor="story">Sinopse</label>
-        <textarea type="text" name="storyline" id="story" value={this.state.storyline}
-          onChange={this.handleInputChange} />
+        <textarea
+          type="text"
+          name="storyline"
+          id="story"
+          value={this.state.storyline}
+          onChange={this.handleInputChange}
+        />
         <label htmlFor="rate">Avaliação</label>
         <input
           type="number"
@@ -56,18 +79,21 @@ class AddMovie extends React.Component {
           value={this.state.rating}
           onChange={this.handleInputChange}
         />
-        
+
         <label htmlFor="genre">Gênero</label>
         <select id="genre" name="genre" value={this.state.genre} onChange={this.handleInputChange}>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
           <option value="thriller">Suspense</option>
         </select>
-        
-        <button value={this.state} onClick={() => {
+
+        <button
+          value={this.state}
+          onClick={() => {
             onClick(this.state);
             this.initialValue();
-          }}>
+          }}
+        >
           Adicionar filme
         </button>
       </form>
