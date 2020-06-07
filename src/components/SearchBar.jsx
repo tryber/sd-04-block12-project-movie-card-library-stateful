2d079989ff983a9c6567a03ca731d96e1fc67070
wistyk <leonardoxw@gmail.com>
2020-06-07 14:39:58 -0300

    CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,10 @@ class SearchBar extends Component {
         <label htmlFor="Inclui o texto">Inclui o texto</label>
         <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
         <label htmlFor="Mostrar somente favoritos">Mostrar somente favoritos</label>
-        <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}
+        <input
+          type="checkbox"
+          checked={this.props.bookmarkedOnly}
+          onChange={this.props.onBookmarkedChange}
         />
         <label htmlFor="Filtrar por gênero">Filtrar por gênero</label>
         <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange} />
