e865a16068592bc543ea5a26fab447a1b09ab655
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-06-06 21:23:14 -0300

    MovieLibray cc

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,25 +1,51 @@
 // implement MovieLibrary component here
-// import React, { Component } from 'react';
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies:this.props.movies,
+    };
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+  updateSearch(event) {
+    this.setState({ searchText: event.target.value });
+  }
 
-// export default MovieLibrary;
+  updateBookMarked(event) {
+    this.setState({ bookmarkedOnly: event.target.checked })
+  }
+
+  updateSelected(event) {
+    this.setState({ selectedGenre: event.target.value })
+  }
+
+  render() {
+    console.log(this)
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.updateSearch.bind(this)}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.updateBookMarket.bind(this)}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.updateSelected.bind(this)}
+        />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
