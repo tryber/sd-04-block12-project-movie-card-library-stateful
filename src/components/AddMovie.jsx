bd74b5c54d877f19b5dac2fb093fbfbb4ce50bea
wistyk <leonardoxw@gmail.com>
2020-06-07 16:51:22 -0300

    CC

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,26 +6,11 @@ class AddMovie extends Component {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.props;
     return (
       <form>
-        <label htmlFor="Título">
-          Título
-          <input type="text" value={title} />
-        </label>
-        <label htmlFor="Subtítulo">
-          Subtítulo
-          <input type="text" value={subtitle} />
-        </label>
-        <label htmlFor="Imagem">
-          Imagem
-          <input type="text" value={imagePath} />
-        </label>
-        <label htmlFor="Sinopse">
-          Sinopse
-          <textarea value={storyline} cols="30" rows="10" />
-        </label>
-        <label htmlFor="Avaliação">
-          Avaliação
-          <input type="number" value={rating} />
-        </label>
+        <label htmlFor="Título">Título<input type="text" value={title} /></label>
+        <label htmlFor="Subtítulo">Subtítulo<input type="text" value={subtitle} /></label>
+        <label htmlFor="Imagem">Imagem<input type="text" value={imagePath} /></label>
+        <label htmlFor="Sinopse">Sinopse<textarea value={storyline} cols="30" rows="10" /></label>
+        <label htmlFor="Avaliação">Avaliação<input type="number" value={rating} /></label>
         <label htmlFor="Gênero">Gênero</label>
         <select value={genre}>
           <option value="action">Ação</option>
