ed8f616008bb167592e16c2bb5eb592e2dbd3852
danilossousa <danilosantossousa@gmail.com>
2020-06-08 08:35:07 -0300

    v0.5.0.1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,13 +12,20 @@ class SearchBar extends Component {
           value={this.props.searchText}
           onChange={this.props.onSearchTextChange}
         />
-        <label htmlFor="check">Mostrar somente favoritos</label>
+        <label htmlFor="check-fav">Mostrar somente favoritos</label>
         <input
-          id="check"
+          id="check-fav"
           type="checkbox"
           checked={this.props.bookmarkedOnly}
           onChange={this.props.onBookmarkedChange}
         />
+        <label htmlFor="gen">Filtrar por gênero</label>
+        <select id="gen" value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
       </form>
     );
   }
