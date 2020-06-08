e542d26db508431bc45bd966e3259441e972e15f
danilossousa <danilosantossousa@gmail.com>
2020-06-08 09:39:22 -0300

    v0.6.1.1

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,11 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import SearchBar from './SearchBar.jsx';
 
 class AddMovie extends Component {
   render() {
     return (
-      <form />
+      <SearchBar />
     );
   }
 }
