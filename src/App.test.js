68d80787c56648bfa33dc3ef86d6f1454e57601d
Lucas Ribas <lucas@mundoribas.com.br>
2020-06-08 10:37:33 -0300

    Componente SearchBar - done

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from './App';
+import Header from './components/Header';
+// import MovieCard from './components/MovieCard';
+// import MovieList from './components/MovieList';
+// import Rating from './components/Rating';
 
 describe('<App /> component', () => {
   it('renders a `Header` component', () => {
