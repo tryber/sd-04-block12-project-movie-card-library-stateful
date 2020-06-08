5d9fd0d6a2991bd50c24f3c78f74ed0efaf9dc25
Pedro Pires <pedrotpoliveira@gmail.com>
2020-06-08 19:23:15 -0300

    SearchBar 3rd Attempt

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'Action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form>
+        <input type="submit" value="Submit" onClick={onClick} />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
