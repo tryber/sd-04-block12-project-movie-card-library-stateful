2d0b1e51a6e777c6ab1cbc170484079c64504baf
fogo5000 <lucaslucas2008@gmail.com>
2020-06-08 02:29:26 -0300

    Pass at 24 initial test in AddMovie 1.0v

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,7 +15,7 @@ export default class AddMovie extends Component {
 
 	funTitle() {
 		return (
-			<label htmlFor="searchText">
+			<label htmlFor="title">
 				Título
 				<input type="text" value={this.state.title}
 					onChange={(event) => this.setState({title:event.target.value})} 
@@ -26,10 +26,43 @@ export default class AddMovie extends Component {
 
 	funSubTitle() {
 		return (
-			<label htmlFor="searchText">
+			<label htmlFor="subTitle">
 				Subtítulo
 				<input type="text" value={this.state.subtitle}
 					onChange={(event) => this.setState({subtitle:event.target.value})} 
+				/>
+			</label>
+		);
+	}
+
+	funImagePath() {
+		return (
+			<label htmlFor="imagePath">
+				Imagem
+				<input type="text" value={this.state.imagePath}
+					onChange={(event) => this.setState({imagePath:event.target.value})} 
+				/>
+			</label>
+		);
+	}
+
+	funSinopse() {
+		return (
+			<label htmlFor="sinopse">
+				Sinopse
+				<textarea value={this.state.storyline}
+					onChange={(event) => this.setState({storyline:event.target.value})} 
+				/>
+			</label>
+		);
+	}
+
+	funRaiting() {
+		return (
+			<label htmlFor="raiting">
+				Avaliação
+				<input type="number" value={this.state.rating}
+					onChange={(event) => this.setState({rating: parseFloat(event.target.value)})} 
 				/>
 			</label>
 		);
@@ -40,6 +73,9 @@ export default class AddMovie extends Component {
 			<form>
 				{this.funTitle()}
 				{this.funSubTitle()}
+				{this.funImagePath()}
+				{this.funSinopse()}
+				{this.funRaiting()}		
 			</form>
 		);
 	}
