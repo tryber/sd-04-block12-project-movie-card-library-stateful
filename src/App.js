494613aacd88c9cbbb5b11ff88626fa099969f37
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-08 13:57:33 -0300

    initiate MovieLibrary component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 import Header from './components/Header';
 
 class App extends Component {
@@ -9,8 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
-        <AddMovie />
+        <MovieLibrary />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,28 @@
-// import React from 'react';
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: '',
+            bookmarkedOnly: false,
+            selectedGenre: '',
+            movies: '',
+        }
+        
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                {/* <MovieList /> */}
+                <AddMovie />
+            </div>
+        );
+    }
+}
+
+export default MovieLibrary;
