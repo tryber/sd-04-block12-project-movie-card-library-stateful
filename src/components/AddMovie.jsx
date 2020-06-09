fc34d03002aebf62fb633a0f30c963e1ff1368a6
wistyk <leonardoxw@gmail.com>
2020-06-09 14:17:03 -0300

    funções criadas no render

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,9 +4,7 @@ import React, { Component } from 'react';
 class AddMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      title: '',
-    };
+    this.state = { title: '' };
 
     this.tito = this.tito.bind(this);
     this.sub = this.sub.bind(this);
@@ -16,6 +14,63 @@ class AddMovie extends Component {
     this.gen = this.gen.bind(this);
   }
 
+  titulo(a, b) {
+    return (
+      <label htmlFor="Título">
+        Título
+        <input type="text" value={a} onChange={(e) => b(e)} />
+      </label>
+    );
+  }
+
+  subtitulo(a, b) {
+    return (
+      <label htmlFor="Subtítulo">
+        Subtítulo
+        <input type="text" value={a} onChange={(e) => b(e)} />
+      </label>
+    );
+  }
+
+  imagem(a, b) {
+    return (
+      <label htmlFor="Imagem">
+        Imagem
+        <input type="text" value={a} onChange={(e) => b(e)} />
+      </label>
+    );
+  }
+
+  story(a, b) {
+    return (
+      <label htmlFor="Sinopse">
+        Sinopse
+        <textarea type="text" cols="30" rows="10" value={a} onChange={(e) => b(e)} />
+      </label>
+    );
+  }
+
+  avalia(a, b) {
+    return (
+      <label htmlFor="Avaliação">
+        Avaliação
+        <input type="number" value={a} onChange={(e) => b(e)} />
+      </label>
+    );
+  }
+
+  gener(a, b) {
+    return (
+      <label htmlFor="Gênero">
+        Gênero
+        <select value={a} onChange={(e) => b(e)}>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspence</option>
+        </select>
+      </label>
+    );
+  }
   tito(e) {
     return this.setState({ title: e.target.value });
   }
@@ -41,37 +96,16 @@ class AddMovie extends Component {
   }
 
   render() {
-    const { title, subtitle, imagePath, storyline, rating, genre } = this.props;
+    const { subtitle, imagePath, storyline, rating, genre } = this.props;
+    const { title } = this.state;
     return (
       <form>
-        <label htmlFor="Título">
-          Título
-          <input type="text" value={title} onChange={this.tito} />
-        </label>
-        <label htmlFor="Subtítulo">
-          Subtítulo
-          <input type="text" value={subtitle} onChange={this.sub} />
-        </label>
-        <label htmlFor="Imagem">
-          Imagem
-          <input type="text" value={imagePath} onChange={this.img} />
-        </label>
-        <label htmlFor="Sinopse">
-          Sinopse
-          <textarea value={storyline} cols="30" rows="10" onChange={this.sty} />
-        </label>
-        <label htmlFor="Avaliação">
-          Avaliação
-          <input type="number" value={rating} onChange={this.rat} />
-        </label>
-        <label htmlFor="Gênero">
-          Gênero
-          <select value={genre} onChange={this.gen}>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspence</option>
-          </select>
-        </label>
+        {this.titulo(title, this.tito)}
+        {this.subtitulo(subtitle, this.sub)}
+        {this.imagem(imagePath, this.img)}
+        {this.story(storyline, this.sty)}
+        {this.avalia(rating, this.rat)}
+        {this.gener(genre, this.gen)}
         <button type="submit">Adicionar filme</button>
       </form>
     );
