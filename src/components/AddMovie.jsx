43b0713aef4b60e57d251ab561cd93c23b32b883
fogo5000 <lucaslucas2008@gmail.com>
2020-06-09 01:08:47 -0300

    Pass at 24 initial test in AddMovie fix CC 1.0v

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			subtitle: '',
-			title: '',
-			imagePath: '',
-			storyline: '',
-			rating: 0,
-			genre: 'action',
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
 
 	funTitle() {
 		return (
@@ -24,59 +24,59 @@ export default class AddMovie extends Component {
 		);
 	}
 
-	funSubTitle() {
-		return (
-			<label htmlFor="subTitle">
-				Subtítulo
-				<input type="text" value={this.state.subtitle}
-					onChange={(event) => this.setState({subtitle:event.target.value})} 
-				/>
-			</label>
-		);
-	}
+  funSubTitle() {
+    return (
+      <label htmlFor="subTitle">
+        Subtítulo
+        <input type="text" value={this.state.subtitle}
+          onChange={(event) => this.setState({subtitle:event.target.value})} 
+        />
+      </label>
+    );
+  }
 
-	funImagePath() {
-		return (
-			<label htmlFor="imagePath">
-				Imagem
-				<input type="text" value={this.state.imagePath}
-					onChange={(event) => this.setState({imagePath:event.target.value})} 
-				/>
-			</label>
-		);
-	}
+  funImagePath() {
+    return (
+      <label htmlFor="imagePath">
+        Imagem
+        <input type="text" value={this.state.imagePath}
+          onChange={(event) => this.setState({imagePath:event.target.value})} 
+        />
+      </label>
+    );
+  }
 
-	funSinopse() {
-		return (
-			<label htmlFor="sinopse">
-				Sinopse
-				<textarea value={this.state.storyline}
-					onChange={(event) => this.setState({storyline:event.target.value})} 
-				/>
-			</label>
-		);
-	}
+  funSinopse() {
+    return (
+      <label htmlFor="sinopse">
+        Sinopse
+        <textarea value={this.state.storyline}
+          onChange={(event) => this.setState({storyline:event.target.value})} 
+        />
+      </label>
+    );
+  }
 
-	funRaiting() {
-		return (
-			<label htmlFor="raiting">
-				Avaliação
-				<input type="number" value={this.state.rating}
-					onChange={(event) => this.setState({rating: parseFloat(event.target.value)})} 
-				/>
-			</label>
-		);
-	}
+  funRaiting() {
+    return (
+      <label htmlFor="raiting">
+        Avaliação
+        <input type="number" value={this.state.rating}
+          onChange={(event) => this.setState({rating: parseFloat(event.target.value)})} 
+        />
+      </label>
+    );
+  }
 
-	render() {
-		return (
-			<form>
-				{this.funTitle()}
-				{this.funSubTitle()}
-				{this.funImagePath()}
-				{this.funSinopse()}
-				{this.funRaiting()}		
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form>
+        {this.funTitle()}
+        {this.funSubTitle()}
+        {this.funImagePath()}
+        {this.funSinopse()}
+        {this.funRaiting()}		
+      </form>
+    );
+  }
 }
