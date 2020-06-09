c82b307a5c9fe84149a7ce5e00414d92de40da44
Julianafatsil <julianafatsil@gmail.com>
2020-06-09 01:54:39 -0300

    Create MovieLibrary Compoent

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,30 +1,49 @@
-// import React, { Component } from 'react';
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//     // inicializo stado das variaveis que sofrerão alteração
-//     this.state = {
-//       // objeto de ações
-//       searchText: '',
-//       bookmarkedOnly: false,
-//       selectedGenre: '',
-//     };
-//     // uso bind quando tenho alteração no meu state / controlador
-//     this.onSearchTextChange = this.onSearchTextChange.bind(this);
-//     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-//     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this); // func sem usar
-//   }
+import React, { Component } from 'react';
 
-//   onSearchTextChange(event) {
-//     const { value } = event.target;
-//     this.setState({ searchText: value }); // recebe o novo value
-//   }
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
-//   onBookmarkedChange() {
-//     this.setState({ bookmarkedOnly: !this.state.bookmarkedOnly });// nega o que vem
-//   }
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    // inicializo stado das variaveis que sofrerão alteração
+    this.state = {
+      // objeto de ações
+      searchText: "",
+      bookmarkedOnly: false,
+      selectedGenre: "",
+      movies: this.props.movies,
+    };
+    // uso bind quando tenho alteração no meu state / controlador
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this); // func sem usar
+  }
 
-//   onSelectedGenreChange(event) {
-//     const { value } = event.target; // pegar valor do campo
-//     this.setState({ selectedGenre: value }); // selectedGenre vale oque usuario seleciona
-//   }
+  onSearchTextChange(event) {
+    const { value } = event.target;
+    this.setState({ searchText: value }); // recebe o novo value
+  }
+
+  onBookmarkedChange() {
+    this.setState({ bookmarkedOnly: !this.state.bookmarkedOnly }); // nega o que vem
+  }
+
+  onSelectedGenreChange(event) {
+    const { value } = event.target; // pegar valor do campo
+    this.setState({ selectedGenre: value }); // selectedGenre vale oque usuario seleciona
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar state={this.state} />
+        <MovieList movies={this.state.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
