939f0a2ae8875438e88c07ecdf22d344cdc75677
douglaza <douglaza@gmail.com>
2020-06-09 22:21:11 -0300

    CC 4th try today

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,7 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
