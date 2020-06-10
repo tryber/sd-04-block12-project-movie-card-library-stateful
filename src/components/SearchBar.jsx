bd312ab0f8c239c533af33cb89330df26f335bce
Kaecio <kaecio@yahoo.com.br>
2020-06-10 00:33:14 -0300

    SearchBar done, UFC 251 CC 1 x 0 Kaecio

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,13 @@
 // implement SearchBar component here
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import PropTypes from "prop-types";
 
 class SearchBar extends Component {
   render() {
-   const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, 
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <form>
         <label htmlFor="text-enclude">
