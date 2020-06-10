a97156d08e083497f2890103223700e3ffed0104
Kaecio <kaecio@yahoo.com.br>
2020-06-10 00:19:33 -0300

    SearchBar done, UFC 251 CC x Kaecio

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,39 +1,27 @@
 // implement SearchBar component here
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
 
 class SearchBar extends Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
-
+   const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form>
-        <label>
-          Inclui o texto:
-          <input type='text' value={searchText} onChange={onSearchTextChange} />
+        <label htmlFor="text-enclude">
+        Inclui o texto:
+          <input type="text" value={searchText} onChange={onSearchTextChange} />
         </label>
-        <label>
+        <label htmlFor="favorite">
           Mostrar somente favoritos
-          <input
-            type='checkbox'
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
-          />
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </label>
-        <label>
+        <label htmlFor="genrer">
           Filtrar por gênero
           <select value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value=''>Todos</option>
-            <option value='action'>Ação</option>
-            <option value='comedy'>Comédia</option>
-            <option value='thriller'>Suspense</option>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
           </select>
         </label>
       </form>
