9562f35f7018dcf2084282e5c41cae0706463de1
Kaecio <kaecio@yahoo.com.br>
2020-06-10 00:43:31 -0300

    SearchBar done, UFC 251 CC 2 x 0 Kaecio

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     const {
-      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, 
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre,
       onSelectedGenreChange,
     } = this.props;
     return (
