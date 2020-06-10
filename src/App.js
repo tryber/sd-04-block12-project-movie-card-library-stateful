273dc0702cdee52dade3450e21139327fef10663
samueap <samueap@gmail.com>
2020-06-10 06:49:24 -0300

    add requirements MovieLibrary&AddMovie

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
+import Header from '../src/components/Header';
+import movies from '../src/data';
+import SearchBar from '../src/components/SearchBar';
+import MovieLibrary from '../src/components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <MovieLibrary movies= {movies} />
     </div>
   );
 }
