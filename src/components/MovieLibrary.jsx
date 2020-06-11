cc8008e3c4a3eb83efe7c8b84b1dbf701d59fe41
wistyk <leonardoxw@gmail.com>
2020-06-11 16:46:45 -0300

    criado os callbacks para searchBar no MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,6 +14,22 @@ class MovieLibrary extends Component {
       selectedGenre: '',
       movies: props.movies,
     };
+
+    this.st = this.st.bind(this);
+    this.bm = this.bm.bind(this);
+    this.sg = this.sg.bind(this);
+  }
+
+  st(e) {
+    return this.setState({ searchText: e.target.value });
+  }
+
+  bm(e) {
+    return this.setState({ bookmarkedOnly: e.target.value });
+  }
+
+  sg(e) {
+    return this.setState({ selectedGenre: e.target.value });
   }
 
   render() {
@@ -23,8 +39,11 @@ class MovieLibrary extends Component {
         <h2> My awesome movie library </h2>
         <SearchBar
           searchText={searchText}
+          onSearchTextChange={this.st}
           bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={this.bm}
           selectedGenre={selectedGenre}
+          onSelectedGenreChange={this.sg}
         />
         <MovieList movies={movies} />
         <AddMovie />
