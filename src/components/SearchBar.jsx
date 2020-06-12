a17950bbc169f959532d556c9d682ff346183160
gabriellukke <gabriellukke@outlook.com>
2020-06-12 19:24:03 -0300

    Search Bar CC3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -22,29 +22,31 @@ const genres = [
 class SearchBar extends React.Component {
   render() {
     return (
-        <form>
-          <label htmlFor="search">Inclui o texto:
-            <input
-              type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}
-            />
-          </label>
-          <label htmlFor="showfav">Mostrar somente favoritos:
-            <input
-              type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}
-            />
-          </label>
-          <label htmlFor="genderfilter">Filtrar por gênero
-            <select
-              type="text" value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}
-            >
-              {genres.map((genre) => (
-                <option key={genre.value} value={genre.value}>
-                  {genre.text}
-                </option>
-              ))}
-            </select>
-          </label>
-        </form>
+      <form>
+        <label htmlFor="search">Inclui o texto:
+          <input
+            type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}
+          />
+        </label>
+        <label htmlFor="showfav">Mostrar somente favoritos:
+          <input
+            type="checkbox" checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange}
+          />
+        </label>
+        <label htmlFor="genderfilter">Filtrar por gênero
+          <select
+            type="text" value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange}
+          >
+            {genres.map((genre) => (
+              <option key={genre.value} value={genre.value}>
+                {genre.text}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
     );
   }
 }
