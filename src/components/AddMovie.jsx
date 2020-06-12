aa3d9169f319987c8cef8a2d2bc749cb3fd1f6b9
Tiago-ers <trsilva284@gmail.com>
2020-06-12 13:55:38 -0300

    cc4 -corrigindo AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,9 @@
 // implement AddMovie component here
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AddMovie extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    //this.AddMovie = this.Movie.bind(this);
     this.state = {
       subtitle: '',
       title: '',
@@ -19,13 +18,13 @@ class AddMovie extends Component {
 
   modState(e) {
     this.setState({
-      title:e.target.value,
-      subtitle:e.target.value,
-      imagePath:e.target.value,
-      storyline:e.target.value,
-      rating:Number(e.target.value),
-      genre:e.target.value,
-    })
+      title: e.target.value,
+      subtitle: e.target.value,
+      imagePath: e.target.value,
+      storyline: e.target.value,
+      rating: Number(e.target.value),
+      genre: e.target.value,
+    });
   }
 
   //Função btnMovie obtên o estado inicial do objeto e passa o onClik pela props
@@ -44,21 +43,21 @@ class AddMovie extends Component {
   render() {
     return (
       <form>
-        <label>Título</label>
-        <input type='text' value={this.state.title} onChange={this.modState}/>
-        <label>Subtítulo</label>
-        <input type='text' value={this.state.subtitle} onChange={this.modState}/>
-        <label>Imagem</label>
-        <input type='text' value={this.state.imagePath} onChange={this.modState}/>
-        <label>Sinopse</label>
-        <textarea value={this.state.storyline} onChange={this.modState}></textarea>
-        <label>Avaliação</label>
-        <input type='number' step='0' value={this.state.rating} onChange={this.modState}/>
-        <label>Gênero</label>
+        <label htmlFor="titulo">Título</label>
+        <input type="text" value={this.state.title} onChange={this.modState} />
+        <label htmlFor="subtitulo">Subtítulo</label>
+        <input type="text" value={this.state.subtitle} onChange={this.modState} />
+        <label htmlFor="imagem">Imagem</label>
+        <input type="text" value={this.state.imagePath} onChange={this.modState} />
+        <label htmlFor="sinopse">Sinopse</label>
+        <textarea value={this.state.storyline} onChange={this.modState} />
+        <label htmlFor="avaliacao">Avaliação</label>
+        <input type="number" step='0' value={this.state.rating} onChange={this.modState} />
+        <label htmlFor="genero">Gênero</label>
         <select value={this.state.genre} onChange={this.modState}>
-          <option value='action'>Ação</option>
-          <option value='comedy'>Comédia</option>
-          <option value='thriller'>Suspense</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
         </select>
         <button onClick={this.btnMovie}>Adicionar filme</button>
       </form>
