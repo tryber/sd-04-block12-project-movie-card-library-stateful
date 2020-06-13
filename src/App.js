20efb9bd868a8d47806d991ecc6c44c09cca6a3c
gabriellukke <gabriellukke@outlook.com>
2020-06-13 12:03:29 -0300

    AddMovie

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from './data';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <SearchBar />
       <MovieList movies={data} />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,84 @@
-// implement AddMovie component here
+import React from 'react';
+
+const genres = [
+  { value: 'action', text: 'Ação' },
+  { value: 'comedy', text: 'Comédia' },
+  { value: 'thriller', text: 'Suspense' },
+];
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.createFormComponent = this.createFormComponent.bind(this);
+    this.resetState = this.resetState.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  };
+
+  handleChange(event, name) {
+    this.setState(() => ({ [name]: event.target.value }))
+    console.log(event.target)
+  }
+
+  createFormComponent(title, name, type = 'text', value) {
+    return (
+      <label htmlFor={name}>
+        {title}
+        <input 
+          name={name} type={type} value={value} onChange={((e) => this.handleChange(e, name))} 
+        />
+      </label>
+    );
+  }
+
+  resetState() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    console.log(title);
+    return (
+      <form>
+        {this.createFormComponent('Título', 'title', title)}
+        {this.createFormComponent('Subtítulo', 'subtitle', subtitle)}
+        {this.createFormComponent('Imagem', 'image', imagePath)}
+        <label htmlFor="storyline">Sinopse
+          <textarea value={storyline} onChange={((e) => this.handleChange(e, 'storyline'))} />
+        </label>
+        {this.createFormComponent('Avaliação', 'rating', 'number', rating)}
+        <label htmlFor="genre">Gênero
+          <select value={genre} onChange={((e) => this.handleChange(e, 'genre'))}>
+            {genres.map((genre) => (
+              <option key={genre.value} value={genre.value}>
+                {genre.text}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="button" onClick={() => this.resetState}>
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
