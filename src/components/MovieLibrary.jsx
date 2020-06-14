082ed81649d0d9fe4bf2ee7b7635bbdd1197628e
fogo5000 <lucaslucas2008@gmail.com>
2020-06-14 20:45:25 -0300

    Pass in 22 tests of MovieLibrary 1.0v

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,6 +3,36 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 
+const m = [
+  {
+    title: 'An awesome title',
+    subtitle: 'Movie Subtitle 1',
+    storyline: 'Movie Storyline 1',
+    rating: 4.5,
+    imagePath: 'images/Ghost_In_The_Shell_2_0.jpg',
+    bookmarked: true,
+    genre: 'action',
+  },
+  {
+    title: 'Movie Title 2',
+    subtitle: 'An incredible subtitle',
+    storyline: 'Movie Storyline 2',
+    rating: 4.5,
+    imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+    bookmarked: false,
+    genre: 'comedy',
+  },
+  {
+    title: 'Movie Title 3',
+    subtitle: 'Movie Subtitle 3',
+    storyline: 'An great storyline',
+    rating: 3,
+    imagePath: 'images/Final_Fantasy_Spirits_Within.jpg',
+    bookmarked: false,
+    genre: 'thriller',
+  },
+];
+
 export default class MovieLibrary extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +42,9 @@ export default class MovieLibrary extends Component {
       bookmarkedOnly: false,
       selectedGenre: '',
       movies,
+      newMovies: movies,
     };
+    this.funTrocaGeral = this.funTrocaGeral.bind(this);
   }
 
   funSearchBarChange(event) {
@@ -27,6 +59,44 @@ export default class MovieLibrary extends Component {
     this.setState({ selectedGenre: event.target.value });
   }
 
+
+  funLowerSplit (ti, subTi, story) {
+    let resposta = [];
+    let aux = '';
+    let lower = '';
+
+    aux = `${ti} ${subTi} ${story}`;
+    lower = aux.toLowerCase();
+    resposta = lower.split(' ');
+    return resposta;
+  }
+
+  funTrocaGeral() {
+    let novo = [];
+    if (this.state.searchText) {
+      this.state.movies.forEach((movie) => {
+        let troca = [];
+        const { title, subtitle, storyline } = movie;
+        troca = this.funLowerSplit(title, subtitle, storyline);
+        if (troca.includes(this.state.searchText.toLowerCase())) {
+          novo.push(movie);
+        }
+      });
+      return novo;
+    } else if (this.state.bookmarkedOnly) {
+      return this.state.movies.filter((movie) => {
+        const { bookmarked } = movie;
+        return this.state.bookmarkedOnly === bookmarked;
+      });
+    } else if (this.state.selectedGenre) {
+      return this.state.movies.filter((movie) => {
+        const { genre } = movie;
+        return this.state.selectedGenre === genre;
+      });
+    }
+    return this.state.movies;
+  }
+
   render() {
     return (
       <div>
@@ -38,7 +108,7 @@ export default class MovieLibrary extends Component {
           selectedGenre={this.state.selectedGenre}
           onSelectedGenreChange={(event) => this.funGenreChange(event)}
         />
-        <MovieList movies={this.state.movies} />
+        <MovieList movies={this.funTrocaGeral()} />
         <AddMovie onClick="" />
       </div>
     );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
+  funTest() {
+    const { movies } = this.props;
+    console.log(movies.length);
+  }
+
   render() {
     const { movies } = this.props;
-
     return (
       <div className="movie-list">
+        {this.funTest()}
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
