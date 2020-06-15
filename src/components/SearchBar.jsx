4aa782286b6c5ecf92ba1aeee090063ae9f0c806
Kaecio <kaecio@yahoo.com.br>
2020-06-15 03:59:58 -0300

    Rendering the AddMovies

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   renderSearch() {
     return (
       <label htmlFor="entrada">
@@ -19,26 +19,28 @@ class SearchBar extends React.Component {
   render() {
     return (
 
-        <form>
-          {this.renderSearch()}
-          <br />
-          <label htmlFor="check">
+      <form>
+        {this.renderSearch()}
+        <br />
+        <label htmlFor="check">
           Mostrar somente favoritos
-            <input
-              type="checkbox"
-              name="check"
-              checked={this.props.bookmarkedOnly}
-              onChange={this.props.onBookmarkedChange}
-            />
-          </label>
-          <label htmlFor="selection"> Filtrar por gênero </label>
-          <select name="selection" value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </form>
+          <input
+            type="checkbox"
+            name="check"
+            checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange}
+          />
+        </label>
+        <label htmlFor="selection"> Filtrar por gênero </label>
+        <select name="selection" 
+          value={this.props.selectedGenre} 
+          onChange={this.props.onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </form>
     );
   }
 }
