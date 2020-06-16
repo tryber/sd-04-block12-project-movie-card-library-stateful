2775c33a3bf6fc8b12acb2fe0c3e98453f721609
binhus <eng.robsonwilli@gmail.com>
2020-06-16 10:42:21 -0300

    CC issues 3

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -20,7 +20,7 @@ class AddMovie extends React.Component {
       this.setState({ [event.target.name]: event.target.value });
     }
   };
-  createImput = (type, name, text) => {
+  createImput (type, name, text) {
     return (
       <div>
         <label htmlFor={name}>{text}</label>
