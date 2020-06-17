fb82f4e7b867ad957ed9e4489f0841cf83816534
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-06-17 10:08:03 -0300

    adding formulary render

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -44,6 +44,30 @@ class AddMovie extends React.Component {
     this.reset();
   }
 
+  render() {
+    return (
+      <form>
+        <label htmlFor="titulo">Título</label>
+        <input type="text" value={this.state.title} onChange={this.modify} />
+        <label htmlFor="subtitulo">Subtítulo</label>
+        <input type="text" value={this.state.subtitle} onChange={this.modify} />
+        <label htmlFor="imagem">Imagem</label>
+        <input type="text" value={this.state.imagePath} onChange={this.modify} />
+        <label htmlFor="sinopse">Sinopse</label>
+        <textarea value={this.state.storyline} onChange={this.modify} />
+        <label htmlFor="avaliacao">Avaliação</label>
+        <input type="number" step="0" value={this.state.rating} onChange={this.modify} />
+        <label htmlFor="genero">Gênero</label>
+        <select value={this.state.genre} onChange={this.modify}>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+        <button onClick={this.button}>Adicionar filme</button>
+      </form>
+    );
+  }
+
 }
 
 export default AddMovie;
