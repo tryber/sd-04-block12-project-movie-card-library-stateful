dd15374a33ebfb2fbaf081ea434b3cdf5ff206f1
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-06-17 11:52:16 -0300

    created component Movie Library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,26 @@
 // implement MovieLibrary component here
+import React from 'react';
+// import MovieList from './MovieList';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+
+    this.addMovies = this.addMovies.bind(this);
+  }
+
+  addMovies(value) {
+    this.setState((state) => ({ movies: state.movies.concat([value]) }));
+  }
+
+}
+
+export default MovieLibrary;
