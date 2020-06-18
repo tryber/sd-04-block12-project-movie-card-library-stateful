8fda9b607c61c9b361b15e9c034ecce383a234ec
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-18 12:49:22 -0300

    MovieList

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import MovieLibrary from './components/MovieLibrary';
 import Header from './components/Header';
+import data from './data';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <MovieLibrary />
+        <MovieLibrary movies={data} />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
-    const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies,
+      movies: this.props.movies,
     };
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.setMovies = this.setMovies(this);
+    this.filterMovies = this.filterMovies.bind(this);
   }
 
   onSearchTextChange(event) {
@@ -30,6 +31,27 @@ class MovieLibrary extends Component {
     this.setState(() => ({ selectedGenre: event.target.value }));
   }
 
+  setMovies(item) {
+    this.setState({ movies: [...this.state.movies, item]})
+  }
+
+  filterMovies() {
+    const { bookmarkedOnly, movies, searchText, selectedGenre } = this.state;
+    let movieArray = [...movies];
+    if(bookmarkedOnly) {
+      movieArray = movieArray.filter(item => item.bookmarked === true);
+    } else if(selectedGenre) {
+      movieArray = movieArray.filter(item => item.genre === selectedGenre);
+    } else if(searchText) {
+      movieArray = movieArray.filter(item => 
+        item.title.includes(searchText) ||
+        item.subtitle.includes(searchText) ||
+        item.storyline.includes(searchText)
+        );
+    }
+    return movieArray;  
+  }
+
   render() {
     return (
       <div>
@@ -39,8 +61,8 @@ class MovieLibrary extends Component {
           onBookmarkedChange={this.onBookmarkedChange}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        {/* <MovieList movies={this.state.movies} /> */}
-        <AddMovie />
+        <MovieList movies={this.filterMovies()} />
+        <AddMovie onClick={this.setMovies} />
       </div>
     );
   }
