82a9b7b0e156ecac671f743ce4ec40c5a542471f
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-06-27 14:48:37 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
-
-import Header from './components/Header';
-import movies from './data';
-import SearchBar from './components/SearchBar';
-import MovieLibrary from './components/MovieLibrary';
+import { MovieDetails, NewMovie, EditMovie, NotFound, MovieList } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SearchBar />
-      <MovieLibrary movies={movies} />
-    </div>
+      <BrowserRouter>
+        <header>
+          {/* <Link to="/index">Filmes</Link> */}
+          {/* <Link to="/movies/:id">movies</Link> */}
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+          {/* <Link to="/movies/:id/edit">EDIT MOVIE</Link> */}
+        </header>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/index" component={MovieList} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
