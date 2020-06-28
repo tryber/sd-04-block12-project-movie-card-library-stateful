fdee040eec89a1449d117d6bdac123d48316eebb
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-28 03:34:01 -0300

    SeachBar completo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,37 +1,34 @@
-// implement SearchBar component here
-/*
-Crie um componente chamado SearchBar
-Esse componente renderizará uma barra com filtros acima da listagem de cartões. Quais cartões serão mostrados no componente MovieList dependerá dos filtros escolhidos. SearchBar deve receber como props:
-
-searchText, uma string
-onSearchTextChange, uma callback
-bookmarkedOnly, um boolean
-onBookmarkedChange, uma callback
-selectedGenre, uma string
-onSelectedGenreChange, uma callback
-*/
 import React from 'react';
 
+const genreOptions = [
+  { text: 'Todos', value: '' },
+  { text: 'Ação', value: 'action' },
+  { text: 'Comédia', value: 'comedy' },
+  { text: 'Suspense', value: 'thriller' },
+];
+
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchText: String,
-    };
-  }
+
   render() {
-    //const searchText = this.props.searchText;
-    const onSearchTextChange = this.props.onSearchTextChange;
-    const bookmarkedOnly = this.props.bookmarkedOnly;
-    const selectedGenre = this.props.selectedGenre;
-    const onSelectedGenreChange = this.props.onSelectedGenreChange;
 
     return (
       <div>
         <form>
           <label>
             Inclui o texto: 
-            <input type="text"></input>
+            <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+          </label>
+          <label>
+            Mostrar somente favoritos
+            <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+          </label>
+          <label>
+            Filtrar por gênero
+            <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+              {genreOptions.map((genre) => (
+                <option key={genre.value} value={genre.value}>{genre.text}</option>
+              ))}
+            </select>
           </label>
         </form>
       </div>
