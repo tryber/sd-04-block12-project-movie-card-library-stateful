47f63e56524bb203dcd2a67764718e24eb326cf4
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-28 03:39:55 -0300

    CC issues

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,25 +8,33 @@ const genreOptions = [
 ];
 
 class SearchBar extends React.Component {
-
   render() {
-
     return (
       <div>
         <form>
-          <label>
-            Inclui o texto: 
-            <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+          <label htmlFor="searchBar">
+            Inclui o texto:
+            <input type="text"
+            value={this.props.searchText}
+            onChange={this.props.onSearchTextChange} />
           </label>
-          <label>
+          <label htmlFor="favoritos">
             Mostrar somente favoritos
-            <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+            <input type="checkbox"
+            checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange} />
           </label>
-          <label>
+          <label htmlFor="genre">
             Filtrar por gênero
-            <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+            <select 
+            value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange}>
               {genreOptions.map((genre) => (
-                <option key={genre.value} value={genre.value}>{genre.text}</option>
+                <option 
+                key={genre.value}
+                value={genre.value}>
+                  {genre.text}
+                </option>
               ))}
             </select>
           </label>
