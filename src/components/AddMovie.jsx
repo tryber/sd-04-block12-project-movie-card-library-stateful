5ce9f67ad90536b20bb3901033f48dcfda0e2a97
saturnian99 <saturnian.dsg@gmail.com>
2020-06-29 20:59:27 -0300

    Implemented AddMovie component form

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,82 @@
-// implement AddMovie component here
+import React from 'react';
+
+const initialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  onClickSubmit() {
+    this.props.onClick(this.state);
+    this.setState(initialState);
+  }
+
+  render() {
+    const {
+      title,
+      subtitle,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
+    return (
+      <form>
+        <label>Título:</label>
+        <input
+          type="text"
+          onChange={(e) => this.setState({ title: e.target.value, })}
+          value={title}
+        ></input>
+        <label>Subtítulo:</label>
+        <input
+          type="text"
+          onChange={(e) => this.setState({ subtitle: e.target.value, })}
+          value={subtitle}
+        ></input>
+        <label>Imagem:</label>
+        <input
+          type="text"
+          onChange={(e) => this.setState({ imagePath: e.target.value, })}
+          value={imagePath}
+        ></input>
+        <label>Sinopse:</label>
+        <textarea
+          onChange={(e) => this.setState({ storyline: e.target.value, })}
+          value={storyline}
+        ></textarea>
+        <label>Avaliação:</label>
+        <input
+          type="number"
+          min="0"
+          max="5"
+          step="0.1"
+          onChange={(e) => this.setState({ rating: e.target.value, })}
+          value={rating}
+        ></input>
+        <label>Gênero:</label>
+        <select
+          onChange={(e) => this.setState({ genre: e.target.value, })}
+          value={genre}
+        >
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+        <button onClick={this.onClickSubmit}>Adicionar filme</button>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,55 +13,32 @@ class SearchBar extends React.Component {
 
     return (
       <form>
-        <label>
-          Inclui o texto:
-          <input
-            type="text"
-            value={searchText}
-            onChange={onSearchTextChange}
-          ></input>
-        </label>
-        <label>
-          Mostrar somente favoritos:
-          <input
-            type="checkbox"
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
-          ></input>
-        </label>
-        <label>
-          Filtrar por gênero:
-          <select
-            value={selectedGenre}
-            checked={bookmarkedOnly}
-            onChange={onSelectedGenreChange}
-          >
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
+        <label>Inclui o texto: </label>
+        <input
+          type="text"
+          value={searchText}
+          onChange={onSearchTextChange}
+        ></input>
+        <label>Mostrar somente favoritos:</label>
+        <input
+          type="checkbox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+        ></input>
+        <label>Filtrar por gênero:</label>
+        <select
+          value={selectedGenre}
+          checked={bookmarkedOnly}
+          onChange={onSelectedGenreChange}
+        >
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
       </form>
     );
   }
 }
-/* A propriedade value do select deve receber o valor da prop selectedGenre;
-
-A propriedade onChange do input deve receber o valor da prop onSelectedGenreChange;
-
-O select deve renderizar quatro tags option, com as opções de filtragem por gênero, na seguinte ordem:
-
-Todos, com o valor "";
-Ação, com o valor action;
-Comédia, com o valor comedy;
-Suspense, com o valor thriller. */
 
 export default SearchBar;
-
-/* searchText, uma string
-onSearchTextChange, uma callback
-bookmarkedOnly, um boolean
-onBookmarkedChange, uma callback
-selectedGenre, uma string
-onSelectedGenreChange, uma callback */
