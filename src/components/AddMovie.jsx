74ec39b5b59e725453bbafa04c7be82542a24c5d
saturnian99 <saturnian.dsg@gmail.com>
2020-06-30 15:40:26 -0300

    Divided AddMovie form into components

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,13 +1,90 @@
 import React from 'react';
 
 const initialState = {
-  subtitle: '',
   title: '',
+  subtitle: '',
   imagePath: '',
   storyline: '',
   rating: 0,
   genre: 'action',
 };
+
+const TitleInput = (title) => (
+  <div>
+    <label htmlFor="title">Título</label>
+    <input
+      id="title"
+      type="text"
+      onChange={(e) => this.setState({ title: e.target.value })}
+      value={title}
+    />
+  </div>
+);
+
+const SubtitleInput = (subtitle) => (
+  <div>
+    <label htmlFor="subtitle">Subtítulo</label>
+    <input
+      id="subtitle"
+      type="text"
+      onChange={(e) => this.setState({ subtitle: e.target.value })}
+      value={subtitle}
+    />
+  </div>
+);
+
+const ImageInput = (imagePath) => (
+  <div>
+    <label htmlFor="imagePath">Imagem</label>
+    <input
+      id="imagePath"
+      type="text"
+      onChange={(e) => this.setState({ imagePath: e.target.value })}
+      value={imagePath}
+    />
+  </div>
+);
+
+const StorylineTextArea = (storyline) => (
+  <div>
+    <label htmlFor="storyline">Sinopse</label>
+    <textarea
+      id="storyline"
+      onChange={(e) => this.setState({ storyline: e.target.value })}
+      value={storyline}
+    />
+  </div>
+);
+
+const RatingInput = (rating) => (
+  <div>
+    <label htmlFor="rating">Avaliação</label>
+    <input
+      id="rating"
+      type="number"
+      min="0"
+      max="5"
+      step="0.1"
+      onChange={(e) => this.setState({ rating: parseFloat(e.target.value) })}
+      value={rating}
+    />
+  </div>
+);
+
+const GenreSelect = (genre) => (
+  <div>
+    <label htmlFor="genre">Gênero</label>
+    <select
+      id="genre"
+      onChange={(e) => this.setState({ genre: e.target.value })}
+      value={genre}
+    >
+      <option value="action">Ação</option>
+      <option value="comedy">Comédia</option>
+      <option value="thriller">Suspense</option>
+    </select>
+  </div>
+);
 
 class AddMovie extends React.Component {
   constructor(props) {
@@ -33,47 +110,12 @@ class AddMovie extends React.Component {
 
     return (
       <form>
-        <label>Título</label>
-        <input
-          type="text"
-          onChange={(e) => this.setState({ title: e.target.value, })}
-          value={title}
-        ></input>
-        <label>Subtítulo</label>
-        <input
-          type="text"
-          onChange={(e) => this.setState({ subtitle: e.target.value, })}
-          value={subtitle}
-        ></input>
-        <label>Imagem</label>
-        <input
-          type="text"
-          onChange={(e) => this.setState({ imagePath: e.target.value, })}
-          value={imagePath}
-        ></input>
-        <label>Sinopse</label>
-        <textarea
-          onChange={(e) => this.setState({ storyline: e.target.value, })}
-          value={storyline}
-        ></textarea>
-        <label>Avaliação</label>
-        <input
-          type="number"
-          min="0"
-          max="5"
-          step="0.1"
-          onChange={(e) => this.setState({ rating: parseFloat(e.target.value), })}
-          value={rating}
-        ></input>
-        <label>Gênero</label>
-        <select
-          onChange={(e) => this.setState({ genre: e.target.value, })}
-          value={genre}
-        >
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
+        <TitleInput title={title} />
+        <SubtitleInput subtitle={subtitle} />
+        <ImageInput imagePath={imagePath} />
+        <StorylineTextArea storyline={storyline} />
+        <RatingInput rating={rating} />
+        <GenreSelect genre={genre} />
         <button onClick={this.onClickSubmit}>Adicionar filme</button>
       </form>
     );
