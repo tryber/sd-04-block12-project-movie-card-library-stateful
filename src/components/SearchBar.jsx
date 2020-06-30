708c46da4e3e76c17e665abfed3d539919b96208
saturnian99 <saturnian.dsg@gmail.com>
2020-06-30 14:05:59 -0300

    Divided SearchBar form in components

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,51 @@
 import React from 'react';
 
+const SearchTextInput = (props) => {
+  const { searchText, onSearchTextChange } = props;
+  return (
+    <div>
+      <label>Inclui o texto: </label>
+      <input
+        type="text"
+        value={searchText}
+        onChange={onSearchTextChange}
+      ></input>
+    </div>
+  );
+}
+
+const BookmarkedOnlyInput = (props) => {
+  const { bookmarkedOnly, onBookmarkedChange } = props;
+  return (
+    <div>
+      <label>Mostrar somente favoritos:</label>
+      <input
+        type="checkbox"
+        checked={bookmarkedOnly}
+        onChange={onBookmarkedChange}
+      ></input>
+    </div>
+  );
+}
+
+const GenreFilterSelect = (props) => {
+  const { selectedGenre, onSelectedGenreChange } = props;
+  return (
+    <div>
+      <label>Filtrar por gênero:</label>
+      <select
+        value={selectedGenre}
+        onChange={onSelectedGenreChange}
+      >
+        <option value="">Todos</option>
+        <option value="action">Ação</option>
+        <option value="comedy">Comédia</option>
+        <option value="thriller">Suspense</option>
+      </select>
+    </div>
+  );
+}
+
 class SearchBar extends React.Component {
   render() {
     const {
@@ -13,29 +59,18 @@ class SearchBar extends React.Component {
 
     return (
       <form>
-        <label>Inclui o texto: </label>
-        <input
-          type="text"
-          value={searchText}
-          onChange={onSearchTextChange}
-        ></input>
-        <label>Mostrar somente favoritos:</label>
-        <input
-          type="checkbox"
-          checked={bookmarkedOnly}
-          onChange={onBookmarkedChange}
-        ></input>
-        <label>Filtrar por gênero:</label>
-        <select
-          value={selectedGenre}
-          checked={bookmarkedOnly}
-          onChange={onSelectedGenreChange}
-        >
-          <option value="">Todos</option>
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
+        <SearchTextInput
+          searchText={searchText}
+          onSearchTextChange={onSearchTextChange}
+        />
+        <BookmarkedOnlyInput
+          bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={onBookmarkedChange}
+        />
+        <GenreFilterSelect
+          selectedGenre={selectedGenre}
+          onSelectedGenreChange={onSelectedGenreChange}
+        />
       </form>
     );
   }
